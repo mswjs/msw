aa8b2942b9ca40351d3e750c6aebcf6278ab816d
Artem Zakharchenko <kettanaito@gmail.com>
2018-12-29 16:55:42 +0100

    Adds "msw" CLI to create Service Worker

--- example/server.js
+++ example/server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'))
 })
 
-app.use(express.static(path.resolve(__dirname, '../')))
+app.use(express.static(path.resolve(__dirname, './')))
 
 app.listen(8090, () => {
   console.log('Server established at http://localhost:8090')

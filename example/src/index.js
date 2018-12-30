a5fc902021da56fdef7b6c4dbd03e7b87b544bf6
Artem Zakharchenko <kettanaito@gmail.com>
2018-12-30 11:58:49 +0100

    Adds usage example with CRA

---
+++ example/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import './mocks'
+
+ReactDOM.render(<App />, document.getElementById('root'))

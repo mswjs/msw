5ca3ffcfcbb00beb33b47d155d49cac6a7b76fca
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-03 18:49:28 +0100

    Adds "ctx.errors()" utility to mock GraphQL errors

--- src/context/data.ts
+++ src/context/data.ts
@@ -2,7 +2,7 @@ import { ResponseTransformer } from '../response'
 
 export type DataContext<T> = (payload: T) => ResponseTransformer
 
-export const data: DataContext<any> = (payload) => {
+export const data: DataContext<Record<string, any>> = (payload) => {
   return (res) => {
     res.headers.set('Content-Type', 'application/json')
     res.body = JSON.stringify({ data: payload })

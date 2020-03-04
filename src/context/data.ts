227451517fc11eaff09941df88870a2506a6d81c
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-04 09:25:55 +0100

    Adds comments to graphql context utilities

--- src/context/data.ts
+++ src/context/data.ts
@@ -2,6 +2,9 @@ import { ResponseTransformer } from '../response'
 
 export type DataContext<T> = (payload: T) => ResponseTransformer
 
+/**
+ * Returns a GraphQL body payload.
+ */
 export const data: DataContext<Record<string, any>> = (payload) => {
   return (res) => {
     res.headers.set('Content-Type', 'application/json')
--- src/context/errors.ts
+++ src/context/errors.ts
@@ -2,6 +2,9 @@ import { GraphQLError } from 'graphql'
 import { ResponseTransformer } from '../response'
 import { json } from './json'
 
+/**
+ * Returns a list of GraphQL errors.
+ */
 export const errors = (
   errorsList: Partial<GraphQLError>[],
 ): ResponseTransformer => {

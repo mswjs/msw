5a4e8e7b5dfd36f73e87dd18b6489162f44b8068
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-08 09:48:47 +0100

    Adds REST URI parameters integration test

---
+++ test/rest-api/params.test.ts
@@ -0,0 +1,27 @@
+import * as path from 'path'
+import { TestAPI, runBrowserWith } from '../support/runBrowserWith'
+
+describe('REST: URI Parameters', () => {
+  let api: TestAPI
+
+  beforeAll(async () => {
+    api = await runBrowserWith(path.resolve(__dirname, 'params.mocks.ts'))
+  })
+
+  afterAll(() => {
+    return api.cleanup()
+  })
+
+  it('should retrieve parameters from the URI', async () => {
+    const REQUEST_URL = 'https://api.github.com/users/octocat/messages/abc-123'
+    api.page.evaluate((url) => fetch(url), REQUEST_URL)
+    const res = await api.page.waitForResponse(REQUEST_URL)
+    const body = await res.json()
+
+    expect(res.status()).toBe(200)
+    expect(body).toEqual({
+      username: 'octocat',
+      messageId: 'abc-123',
+    })
+  })
+})

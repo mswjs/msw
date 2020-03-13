18e4f7946433397e3e6dc7b9d4a8911bc8b770c7
guanwei <riverhohai@gmail.com>
2020-03-13 12:06:18 +0800

    Adds cli error parameter prompts and usage examples

--- cli/msw.js
+++ cli/msw.js
@@ -7,13 +7,16 @@ yargs
     'init <publicDir>',
     'Initializes Mock Service Worker at the specified directory',
     (yargs) => {
-      yargs.positional('publicDir', {
-        type: 'string',
-        description: 'Relative path to server public directory',
-        required: true,
-        normalize: true,
-      })
+      yargs
+        .positional('publicDir', {
+          type: 'string',
+          description: 'Relative path to server public directory',
+          required: true,
+          normalize: true,
+        })
+        .example('init', 'msw init public')
     },
     require('./init'),
   )
+  .demandCommand()
   .help().argv

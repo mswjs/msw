46c04ab5995c67ff14f8f87fd0d23f67dea4d891
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-27 10:17:41 +0100

    Aligns "msw init" command with SW updates

--- cli/init.js
+++ cli/init.js
@@ -2,9 +2,11 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 const invariant = require('./invariant')
+const { SERVICE_WORKER_BUILD_PATH } = require('../config/constants')
+
 const cwd = process.cwd()
 
-module.exports = function(args) {
+module.exports = function init(args) {
   const { publicDir } = args
   const resolvedPublicDir = path.resolve(cwd, publicDir)
   const dirExists = fs.existsSync(resolvedPublicDir)
@@ -16,19 +18,20 @@ module.exports = function(args) {
   )
 
   console.log(
-    chalk.gray('Creating Mock Service Worker at "%s"...'),
+    'Initializing the Mock Service Worker at "%s"...',
     resolvedPublicDir,
   )
 
-  const swSrcFilepath = path.resolve(__dirname, '../lib/mockServiceWorker.js')
-  const swFilename = path.basename(swSrcFilepath)
+  const swFilename = path.basename(SERVICE_WORKER_BUILD_PATH)
   const swDestFilepath = path.resolve(resolvedPublicDir, swFilename)
-  fs.copyFile(swSrcFilepath, swDestFilepath, (error) => {
+  fs.copyFile(SERVICE_WORKER_BUILD_PATH, swDestFilepath, (error) => {
     invariant(typeof error !== null, 'Failed to copy Service Worker. %s', error)
 
     console.log(`
 ${chalk.green('Service Worker successfully created!')}
-Continue by creating a mocking module and starting the Service Worker:
+${chalk.gray(swDestFilepath)}
+
+Continue by creating a mocking definition module in your application:
 
   ${chalk.cyan.bold('https://redd.gitbook.io/msw/getting-started#define-mocks')}
 `)

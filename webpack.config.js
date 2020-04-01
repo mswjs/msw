f047de4fe02dbf26b17ae05dfc5075bfe7df0df6
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-01 16:50:43 +0200

    Uses "ts-loader" to transpile TypeScript modules

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             loader: 'babel-loader',
           },
           {
-            loader: 'awesome-typescript-loader',
+            loader: 'ts-loader',
           },
         ],
       },

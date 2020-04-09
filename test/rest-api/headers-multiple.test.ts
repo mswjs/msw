e5c912309ac8554f21dee337b397aa868468424a
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-09 10:34:21 +0200

    set: Adds integration test for handling a request header with multiple values

--- test/rest-api/headers-multiple.test.ts
+++ test/rest-api/headers-multiple.test.ts
@@ -14,17 +14,42 @@ describe('REST: Multiple headers with the same name', () => {
     return api.cleanup()
   })
 
-  it('should receive all the headers', async () => {
-    const REQUEST_URL = 'https://test.msw.io/'
-    api.page.evaluate((url) => fetch(url), REQUEST_URL)
-    const res = await api.page.waitForResponse(REQUEST_URL)
-    const headers = res.headers()
-    const body = await res.json()
+  describe('given handling a request header with multiple values', () => {
+    it('should receive all the headers', async () => {
+      const REQUEST_URL = 'https://test.msw.io/'
+      api.page.evaluate((url) => {
+        const headers = new Headers()
+        headers.append('accept', 'application/json')
+        headers.append('accept', 'image/png')
 
-    expect(res.status()).toEqual(200)
-    expect(headers).toHaveProperty('accept', 'application/json, image/png')
-    expect(body).toEqual({
-      mocked: true,
+        return fetch(url, {
+          method: 'POST',
+          headers,
+        })
+      }, REQUEST_URL)
+      const res = await api.page.waitForResponse(REQUEST_URL)
+      const body = await res.json()
+
+      expect(res.status()).toEqual(200)
+      expect(body).toEqual({
+        accept: 'application/json, image/png',
+      })
+    })
+  })
+
+  describe('given mocking a header with multiple values', () => {
+    it('should receive all the headers', async () => {
+      const REQUEST_URL = 'https://test.msw.io/'
+      api.page.evaluate((url) => fetch(url), REQUEST_URL)
+      const res = await api.page.waitForResponse(REQUEST_URL)
+      const headers = res.headers()
+      const body = await res.json()
+
+      expect(res.status()).toEqual(200)
+      expect(headers).toHaveProperty('accept', 'application/json, image/png')
+      expect(body).toEqual({
+        mocked: true,
+      })
     })
   })
 })

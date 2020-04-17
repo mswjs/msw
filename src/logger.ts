690865a476f7e4671119096560e9adade2af492d
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-17 20:16:15 +0200

    Logs matched requests into browser console

---
+++ src/logger.ts
@@ -0,0 +1,38 @@
+import url from 'url'
+import { MockedRequest, RequestHandler } from './handlers/requestHandler'
+import { MockedResponse } from './response'
+import { getTimestamp } from './utils/getTimestamp'
+import { styleStatusCode } from './utils/styleStatusCode'
+
+export const log = (
+  req: MockedRequest,
+  res: MockedResponse,
+  handler: RequestHandler<any>,
+) => {
+  const isLocal = req.url.startsWith(req.referrer)
+  const parsedUrl = url.parse(req.url)
+  const publicUrl = isLocal
+    ? parsedUrl.pathname
+    : url.format({
+        protocol: parsedUrl.protocol,
+        host: parsedUrl.host,
+        pathname: parsedUrl.pathname,
+      })
+
+  console.groupCollapsed(
+    '[MSW] %s %s %s (%c%s%c)',
+    getTimestamp(),
+    req.method,
+    publicUrl,
+    styleStatusCode(res.status),
+    res.status,
+    'color:inherit',
+  )
+  console.log('Request', req)
+  console.log('Handler:', {
+    mask: handler.mask,
+    resolver: handler.resolver,
+  })
+  console.log('Response', res)
+  console.groupEnd()
+}
---
+++ src/utils/styleStatusCode.ts
@@ -0,0 +1,11 @@
+export const styleStatusCode = (status: number) => {
+  if (status < 300) {
+    return 'color:#69AB32;'
+  }
+
+  if (status < 400) {
+    return 'color:#F0BB4B;'
+  }
+
+  return 'color:#E95F5D;'
+}

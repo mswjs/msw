3a7cac45f7c1b1116e6848fba69ffdfbb2c92160
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-24 22:42:27 +0200

    Uses explicit exports instead of "export * as"

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import * as context from './context'
+
 export { composeMocks } from './composeMocks/composeMocks'
 export { MockedResponse, ResponseTransformer, response } from './response'
-export * as context from './context'
+export { context }
 
 /* Request handlers */
-export * from './handlers/requestHandler'
+export {
+  MockedRequest,
+  RequestHandler,
+  RequestParams,
+  RequestQuery,
+  ResponseResolver,
+  defaultContext,
+} from './handlers/requestHandler'
 export { default as rest, restContext, RESTMethods } from './handlers/rest'
 export { default as graphql, graphqlContext } from './handlers/graphql'

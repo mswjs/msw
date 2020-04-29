865c443bb7f3a058abde41e9bd55f7209edebb90
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-29 11:33:51 +0200

    Reloads client with registrations but no controller

--- src/composeMocks/start/utils/getWorkerInstance.ts
+++ src/composeMocks/start/utils/getWorkerInstance.ts
@@ -10,6 +10,31 @@ export const getWorkerInstance = async (
   url: string,
   options?: RegistrationOptions,
 ): Promise<ServiceWorkerInstanceTuple | null> => {
+  // Resolve the absolute Service Worker URL
+  const absoluteWorkerUrl = (location.origin + url).replace(
+    /(\.\/|\/{2,})/g,
+    '/',
+  )
+
+  const [, mockRegistrations] = await until(async () => {
+    const registrations = await navigator.serviceWorker.getRegistrations()
+    return registrations.filter((registration) => {
+      const worker = getWorkerByRegistration(registration)
+      // Filter out other workers that can be associated with this page
+      return worker?.scriptURL === absoluteWorkerUrl
+    })
+  })
+
+  if (!navigator.serviceWorker.controller && mockRegistrations.length > 0) {
+    // Reload the page when it has associated workers, but no active controller.
+    // The absence of a controller can mean either:
+    // - page has no Service Worker associated with it
+    // - page has been hard-reloaded and its workers won't be used until the next reload.
+    // Since we've checked that there are registrations associated with this page,
+    // at this point we are sure it's hard reload that falls into this clause.
+    location.reload()
+  }
+
   const [, existingRegistration] = await until(() => {
     return navigator.serviceWorker.getRegistration(url)
   })

d10813c64806ac8383c4cb9d98c7a92385da954d
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-01 16:38:02 +0200

    Fixes service worker file path to be relative to __dirname

--- config/constants.js
+++ config/constants.js
@@ -2,12 +2,14 @@ const path = require('path')
 const packageJson = require('../package.json')
 
 const SERVICE_WORKER_SOURCE_PATH = path.resolve(
-  process.cwd(),
+  __dirname,
+  '../',
   'src/mockServiceWorker.js',
 )
 
 const SERVICE_WORKER_BUILD_PATH = path.resolve(
-  process.cwd(),
+  __dirname,
+  '../',
   path.dirname(packageJson.main),
   path.basename(SERVICE_WORKER_SOURCE_PATH),
 )

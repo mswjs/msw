6d01da8d406b32e7b566beadf56d85eb65d52ffc
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-06 10:30:46 +0200

    Adds a "getAbsoluteWorkerUrl" utility

---
+++ src/utils/getAbsoluteWorkerUrl.test.ts
@@ -0,0 +1,11 @@
+import { getAbsoluteWorkerUrl } from './getAbsoluteWorkerUrl'
+
+describe('getAbsoluteWorkerUrl', () => {
+  describe('given a relative worker URL', () => {
+    it('should return an absolute URL against the current location', () => {
+      expect(getAbsoluteWorkerUrl('./mockServiceWorker.js')).toEqual(
+        'http://localhost/mockServiceWorker.js',
+      )
+    })
+  })
+})

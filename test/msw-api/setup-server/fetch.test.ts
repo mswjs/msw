b2e9dd5dea2aa9eff7a29e04120faa8b8c77b2eb
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-12 11:31:02 +0200

    Asserts "Content-Type" header in fetch unit test of "setupServer"

--- test/msw-api/setup-server/fetch.test.ts
+++ test/msw-api/setup-server/fetch.test.ts
@@ -7,6 +7,7 @@ describe('setupServer / fetch', () => {
     rest.get('http://test.msw.io', (req, res, ctx) => {
       return res(
         ctx.status(401),
+        ctx.set('x-header', 'yes'),
         ctx.json({
           firstName: 'John',
           age: 32,
@@ -23,22 +24,23 @@ describe('setupServer / fetch', () => {
     return mock.close()
   })
 
-  describe('given I perform a fetch request in node environment', () => {
+  describe('given I perform a fetch request', () => {
     let res: Response
 
     beforeAll(async () => {
       res = await fetch('http://test.msw.io')
     })
 
-    it('should return a mocked status code', async () => {
+    it('should return mocked status code', async () => {
       expect(res.status).toEqual(401)
     })
 
-    it('should return proper mocked headers', () => {
+    it('should return mocked headers', () => {
       expect(res.headers.get('content-type')).toEqual('application/json')
+      expect(res.headers.get('x-header')).toEqual('yes')
     })
 
-    it('should return a mocked response body', async () => {
+    it('should return mocked body', async () => {
       const body = await res.json()
 
       expect(body).toEqual({

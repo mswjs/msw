78311faf03d18c6a4097cc0687c8f87c84b64d5e
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-14 10:13:14 +0200

    Adds "http.get"/"http.request" unit test with "setupServer"

---
+++ test/msw-api/setup-server/http.test.ts
@@ -0,0 +1,84 @@
+/**
+ * @jest-environment node
+ */
+import * as http from 'http'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+describe('setupServer / http', () => {
+  const mock = setupServer(
+    rest.get('http://test.msw.io', (req, res, ctx) => {
+      return res(
+        ctx.status(401),
+        ctx.set('x-header', 'yes'),
+        ctx.json({
+          firstName: 'John',
+        }),
+      )
+    }),
+  )
+
+  beforeAll(() => {
+    mock.open()
+  })
+
+  afterAll(() => {
+    mock.close()
+  })
+
+  describe('given I perform a request using http.get', () => {
+    let res: http.IncomingMessage
+    let resBody: string = ''
+
+    beforeAll((done) => {
+      http.get('http://test.msw.io', (message) => {
+        res = message
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => (resBody += chunk))
+        res.on('end', done)
+      })
+    })
+
+    it('should return mocked status code', () => {
+      expect(res.statusCode).toEqual(401)
+    })
+
+    it('should return mocked headers', () => {
+      expect(res.headers).toHaveProperty('content-type', 'application/json')
+      expect(res.headers).toHaveProperty('x-header', 'yes')
+    })
+
+    it('should return mocked body', () => {
+      expect(resBody).toEqual('{"firstName":"John"}')
+    })
+  })
+
+  describe('given I perform a request using http.request', () => {
+    let res: http.IncomingMessage
+    let resBody: string = ''
+
+    beforeAll((done) => {
+      const req = http.request('http://test.msw.io', (message) => {
+        res = message
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => (resBody += chunk))
+        res.on('end', done)
+      })
+
+      req.end()
+    })
+
+    it('should return mocked status code', () => {
+      expect(res.statusCode).toEqual(401)
+    })
+
+    it('should return mocked headers', () => {
+      expect(res.headers).toHaveProperty('content-type', 'application/json')
+      expect(res.headers).toHaveProperty('x-header', 'yes')
+    })
+
+    it('should return mocked body', () => {
+      expect(resBody).toEqual('{"firstName":"John"}')
+    })
+  })
+})

bf18136f1cf88a215bbf7e4d744a7a579f8d3d8c
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-25 23:41:40 +0200

    mockServiceWorker: Updates the repository URL

--- src/mockServiceWorker.js
+++ src/mockServiceWorker.js
@@ -1,6 +1,6 @@
 /**
  * Mock Service Worker.
- * @see https://github.com/open-draft/msw
+ * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
  */

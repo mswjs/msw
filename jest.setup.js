612deac693669f679e3743b068905f70a1d8b019
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-04 09:37:53 +0200

    Uses "Headers" and "serviceWorker" definition in browser-like tests only

--- jest.setup.js
+++ jest.setup.js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch')
 
-/**
- * Provide "Headers" to be accessible in test cases
- * since they are not, by default.
- */
-Object.defineProperty(window, 'Headers', {
-  writable: true,
-  value: fetch.Headers,
-})
+if (typeof window !== 'undefined') {
+  // Provide "Headers" to be accessible in test cases
+  // since they are not, by default.
+  Object.defineProperty(window, 'Headers', {
+    writable: true,
+    value: fetch.Headers,
+  })
 
-Object.defineProperty(navigator, 'serviceWorker', {
-  writable: false,
-  value: {
-    addEventListener: () => null,
-  },
-})
+  Object.defineProperty(navigator || {}, 'serviceWorker', {
+    writable: false,
+    value: {
+      addEventListener: () => null,
+    },
+  })
+}

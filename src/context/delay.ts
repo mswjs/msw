4b8d1fd92a7fdbe0e2419c8d67caa1d666474f8d
Usama Hameed <usama.hameed@yahoo.com>
2020-06-04 17:42:36 +0200

    add realistic server response delay to ctx.delay()

--- src/context/delay.ts
+++ src/context/delay.ts
@@ -1,13 +1,22 @@
 import { ResponseTransformer } from '../response'
 
+export const MIN_SERVER_RESPONSE_TIME = 100
+export const MAX_SERVER_RESPONSE_TIME = 400
+
+const getRandomServerResponseTime = () =>
+  Math.floor(
+    Math.random() * (MAX_SERVER_RESPONSE_TIME - MIN_SERVER_RESPONSE_TIME) +
+      MIN_SERVER_RESPONSE_TIME,
+  )
+
 /**
  * Delays the current response for the given duration (in ms)
  * @example
  * res(delay(1500), json({ foo: 'bar' }))
  */
-export const delay = (durationMs: number): ResponseTransformer => {
+export const delay = (durationMs?: number): ResponseTransformer => {
   return (res) => {
-    res.delay = durationMs
+    res.delay = durationMs ?? getRandomServerResponseTime()
     return res
   }
 }

5defbf2b4f4d7ae1503cb68a4e7be14e994b3f7d
Usama Hameed <usama.hameed@yahoo.com>
2020-06-04 17:42:36 +0200

    add realistic server response delay to ctx.delay()

--- src/context/delay.test.ts
+++ src/context/delay.test.ts
@@ -1,4 +1,8 @@
-import { delay } from './delay'
+import {
+  delay,
+  MIN_SERVER_RESPONSE_TIME,
+  MAX_SERVER_RESPONSE_TIME,
+} from './delay'
 import { response } from '../response'
 
 describe('delay', () => {
@@ -6,6 +10,14 @@ describe('delay', () => {
     it('should set delay on the response', () => {
       const endResponse = response(delay(1200))
       expect(endResponse).toHaveProperty('delay', 1200)
+    })
+  })
+
+  describe('if no delay provided ', () => {
+    it('should set random delay on the response', () => {
+      const endResponse = response(delay())
+      expect(endResponse.delay).toBeGreaterThanOrEqual(MIN_SERVER_RESPONSE_TIME)
+      expect(endResponse.delay).toBeLessThanOrEqual(MAX_SERVER_RESPONSE_TIME)
     })
   })
 })

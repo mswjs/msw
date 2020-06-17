564e940fc39048abb08ecddc88ebb955c5b8cb93
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-18 01:51:44 +0200

    Add integration test for binary response types

---
+++ test/rest-api/binary.mocks.ts
@@ -0,0 +1,9 @@
+import { setupWorker, rest } from 'msw'
+
+const worker = setupWorker(
+  rest.get('https://test.msw.io/binary', (_, res, ctx) => {
+    return res(ctx.body(new Uint8Array([1, 2, 3, 4, 5, 6])))
+  }),
+)
+
+worker.start()
---
+++ test/rest-api/binary.test.ts
@@ -0,0 +1,29 @@
+import * as path from 'path'
+import { TestAPI, runBrowserWith } from '../support/runBrowserWith'
+
+describe('REST: Binary response types', () => {
+  let test: TestAPI
+
+  beforeAll(async () => {
+    test = await runBrowserWith(path.resolve(__dirname, 'binary.mocks.ts'))
+  })
+
+  afterAll(() => {
+    return test.cleanup()
+  })
+
+  describe('given a binary response', () => {
+    it('should return a binary response', async () => {
+      const res = await test.request({
+        url: 'https://test.msw.io/binary',
+      })
+      const status = res.status()
+      const headers = res.headers()
+      const body = await res.buffer()
+
+      expect(status).toBe(200)
+      expect(headers).toHaveProperty('x-powered-by', 'msw')
+      expect(new Uint8Array(body)).toEqual(new Uint8Array([1, 2, 3, 4, 5, 6]))
+    })
+  })
+})

bc9286fb16b1f0f0f79fde896b492a425cca5f38
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-18 11:21:44 +0200

    Update test domain

--- test/rest-api/binary.mocks.ts
+++ test/rest-api/binary.mocks.ts
@@ -1,7 +1,7 @@
 import { setupWorker, rest } from 'msw'
 
 const worker = setupWorker(
-  rest.get('https://test.msw.io/binary', (_, res, ctx) => {
+  rest.get('https://test.mswjs.io/binary', (_, res, ctx) => {
     return res(ctx.body(new Uint8Array([1, 2, 3, 4, 5, 6])))
   }),
 )

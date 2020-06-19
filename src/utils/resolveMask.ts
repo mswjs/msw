1dd453f4421eb987d058675343574f9198156f31
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-19 14:41:10 +0200

    rest: Ignores URL query parameters during matching

---
+++ src/utils/resolveMask.ts
@@ -0,0 +1,22 @@
+import { Mask } from '../setupWorker/glossary'
+import { resolveRelativeUrl } from './resolveRelativeUrl'
+
+/**
+ * Converts a given request handler mask into a URL, if given a valid URL string.
+ * Otherwise, returns the mask as-is.
+ */
+export function resolveMask(mask: Mask): URL | RegExp | string {
+  if (mask instanceof RegExp) {
+    return mask
+  }
+
+  try {
+    // Attempt to create a URL instance out of the mask string.
+    // Resolve mask to an absolute URL, because even a valid relative URL
+    // cannot be converted into the URL instance (required absolute URL only).
+    return new URL(resolveRelativeUrl(mask))
+  } catch (error) {
+    // Otherwise, the mask is a path string.
+    return mask
+  }
+}
--- src/utils/resolveRelativeUrl.ts
+++ src/utils/resolveRelativeUrl.ts
@@ -4,12 +4,12 @@ import { Mask } from '../setupWorker/glossary'
  * Resolves a relative URL to the absolute URL with the same hostname.
  * Ignores regular expressions.
  */
-export const resolveRelativeUrl = (mask: Mask) => {
+export const resolveRelativeUrl = <T extends Mask>(mask: T): T => {
   // Global `location` object doesn't exist in Node.
   // Relative request predicate URL cannot become absolute.
   const hasLocation = typeof location !== 'undefined'
 
   return typeof mask === 'string' && mask.startsWith('/')
-    ? `${hasLocation ? location.origin : ''}${mask}`
+    ? ((`${hasLocation ? location.origin : ''}${mask}` as string) as T)
     : mask
 }

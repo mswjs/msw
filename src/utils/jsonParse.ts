563b423bd94b0846c395bd633c2dc5398ddb9428
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-20 17:27:15 +0200

    Annotates "req.body" to be potentially undefined, reuses "jsonParse" in "getJsonBody" utility

--- src/utils/jsonParse.ts
+++ src/utils/jsonParse.ts
@@ -1,4 +1,10 @@
-export function jsonParse<T>(str: string): T | undefined {
+/**
+ * Parses a given string into a JSON.
+ * Does not throw an exception on an invalid JSON string.
+ */
+export function jsonParse<T extends Record<string, any>>(
+  str: string,
+): T | undefined {
   try {
     return JSON.parse(str)
   } catch (error) {
--- src/utils/parseRequestBody.ts
+++ src/utils/parseRequestBody.ts
@@ -8,9 +8,13 @@ export function parseRequestBody(
   if (body) {
     // If the intercepted request's body has a JSON Content-Type
     // parse it into an object, otherwise leave as-is.
-    const isJsonBody = headers?.get('content-type')?.includes('json')
+    const hasJsonContent = headers?.get('content-type')?.includes('json')
 
-    return isJsonBody && typeof body !== 'object' ? getJsonBody(body) : body
+    if (hasJsonContent && typeof body !== 'object') {
+      return getJsonBody(body)
+    }
+
+    return body
   }
 
   // Return whatever falsey body value is given.

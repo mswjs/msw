763859ff9fee4b334f038a7d4565c64eb3cd46f6
wangao <wa17@mails.tsinghua.edu.cn>
2020-06-30 19:16:48 +0800

    fix(isNodeProcess): avoid undefined error

--- src/utils/isNodeProcess.ts
+++ src/utils/isNodeProcess.ts
@@ -2,5 +2,5 @@
  * Returns a boolean indicating if the current process is running in NodeJS environment.
  */
 export function isNodeProcess() {
-  return Object.prototype.toString.call(global.process) === '[object process]'
+  return typeof process === 'object'
 }

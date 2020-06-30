146ad26679bb37a9945255be2fe63db64c66f5af
Jens Meindertsma <jens@jensmeindertsma.com>
2020-06-30 13:55:16 +0200

    Modified helper and corresponding test as suggested

--- test/msw-api/res/network-error.mocks.ts
+++ test/msw-api/res/network-error.mocks.ts
@@ -1,8 +1,8 @@
 import { setupWorker, rest } from 'msw'
 
 const worker = setupWorker(
-  rest.get('http://test.io', (_, res) => {
-    return res.networkError()
+  rest.get('http://test.io/user', (_, res) => {
+    return res.networkError('Network error')
   }),
 )
 

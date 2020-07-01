28dec5df71f5b8a5cd328194d652368d2ab0ce8a
Jens Meindertsma <jens@jensmeindertsma.com>
2020-07-01 17:34:36 +0200

    Added missing `req` parameter to `CustomFunction` type

--- src/onUnhandledRequest.ts
+++ src/onUnhandledRequest.ts
@@ -1,3 +1,5 @@
-type CustomFunction = () => void
+import { MockedRequest } from './handlers/requestHandler'
+
+type CustomFunction = (req: MockedRequest) => void
 
 export type OnUnhandledRequest = 'bypass' | 'warn' | 'error' | CustomFunction

32b7545a04106e8b5e494e7861ae6d009d8fcc97
wangao <1366463855@qq.com>
2020-07-02 16:26:50 +0800

    fix(isNodeProcess): check browser, nodejs and react native environment

--- src/utils/isNodeProcess.ts
+++ src/utils/isNodeProcess.ts
@@ -1,6 +1,18 @@
 /**
  * Returns a boolean indicating if the current process is running in NodeJS environment.
  */
+// Please see https://github.com/mswjs/msw/pull/255
 export function isNodeProcess() {
-  return typeof process === 'object'
+  if (typeof global !== 'object') {
+    // check browser environment
+    return false
+  }
+
+  if (
+    Object.prototype.toString.call(global.process) === '[object process]' ||
+    navigator.product === 'ReactNative'
+  ) {
+    // check nodejs or react native environment
+    return true
+  }
 }

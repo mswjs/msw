67da59662ed2fb753a57dc9e34ea8d87c886a621
wangao <1366463855@qq.com>
2020-07-03 06:41:00 +0800

    test(isNodeProcess): move mock in beforeAll and afterAll hook

--- src/utils/isNodeProcess.react-native.test.ts
+++ src/utils/isNodeProcess.react-native.test.ts
@@ -1,25 +1,47 @@
 // @ts-nocheck
 /**
+ * @jest-environment jsdom
+ */
+/**
  * Test in React Native Environment
+ * Please see https://github.com/mswjs/msw/pull/255
  */
 
 import { isNodeProcess } from './isNodeProcess'
 
-test('returns true when run in a react native environment', () => {
-  const originalProcess = global.process
+// need to mock process and navigator in React Native and restore them
+let originalNavigator
+let originalProcess
+
+beforeAll(() => {
+  // back up
+  originalNavigator = global.navigator
+  originalProcess = global.process
+
+  /**
+   * because it runs in node, Object.proyotype.toString(global.process) will equal '[object process]',
+   * we need to mock global.process and global.navigator. let them behave like in React Native
+   * PLease see https://github.com/facebook/react-native/blob/master/Libraries/Core/setUpNavigator.js and
+   * https://github.com/facebook/react-native/blob/master/Libraries/Core/setUpGlobals.js
+   */
   global.process = {
     env: {
       NODE_ENV: 'development',
     },
   }
-
-  const originalNavigator = global.navigator
   Object.defineProperty(global.navigator, 'product', {
     get: () => {
       return 'ReactNative'
     },
   })
+})
+
+test('returns true when run in a react native environment', () => {
   expect(isNodeProcess()).toBe(true)
+})
+
+afterAll(() => {
+  // restore them
   global.process = originalProcess
   global.navigator = originalNavigator
 })

06aea67665850eb1582502a375d8cab00583b26b
Artem Zakharchenko <kettanaito@gmail.com>
2020-07-03 11:17:58 +0200

    Adds the "getRequestCookies" utility

---
+++ src/utils/request/getRequestCookies.ts
@@ -0,0 +1,28 @@
+import cookieUtils from 'cookie'
+import { MockedRequest } from '../../handlers/requestHandler'
+
+function getAllCookies() {
+  return cookieUtils.parse(document.cookie)
+}
+
+/**
+ * Returns relevant document cookies based on the request `credentials` option.
+ */
+export function getRequestCookies(req: MockedRequest) {
+  switch (req.credentials) {
+    case 'same-origin': {
+      // Return document cookies only when requested a resource
+      // from the same origin as the current document.
+      return location.origin === req.url.origin ? getAllCookies() : {}
+    }
+
+    case 'include': {
+      // Return all document cookies.
+      return getAllCookies()
+    }
+
+    default: {
+      return {}
+    }
+  }
+}

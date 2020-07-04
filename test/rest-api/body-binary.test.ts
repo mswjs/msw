ec9ec0fab7ef1832cf193f66d6f5b9bb853b5547
Artem Zakharchenko <kettanaito@gmail.com>
2020-07-04 11:05:45 +0200

    Uses mocked image binary response for the integration test

--- test/rest-api/body-binary.test.ts
+++ test/rest-api/body-binary.test.ts
@@ -1,29 +1,32 @@
+import * as fs from 'fs'
 import * as path from 'path'
 import { TestAPI, runBrowserWith } from '../support/runBrowserWith'
 
-describe('REST: Binary response types', () => {
-  let test: TestAPI
+let runtime: TestAPI
 
-  beforeAll(async () => {
-    test = await runBrowserWith(path.resolve(__dirname, 'body-binary.mocks.ts'))
+beforeAll(async () => {
+  runtime = await runBrowserWith(
+    path.resolve(__dirname, 'body-binary.mocks.ts'),
+  )
+})
+
+afterAll(() => {
+  return runtime.cleanup()
+})
+
+it('returns given binary in the mocked response', async () => {
+  const res = await runtime.request({
+    url: `${runtime.origin}/images/abc-123`,
   })
+  const status = res.status()
+  const headers = res.headers()
+  const body = await res.buffer()
 
-  afterAll(() => {
-    return test.cleanup()
-  })
+  const expectedBuffer = fs.readFileSync(
+    path.resolve(__dirname, '../fixtures/image.jpg'),
+  )
 
-  describe('given a binary response', () => {
-    it('should return a binary response', async () => {
-      const res = await test.request({
-        url: 'https://test.mswjs.io/binary',
-      })
-      const status = res.status()
-      const headers = res.headers()
-      const body = await res.buffer()
-
-      expect(status).toBe(200)
-      expect(headers).toHaveProperty('x-powered-by', 'msw')
-      expect(new Uint8Array(body)).toEqual(new Uint8Array([1, 2, 3, 4, 5, 6]))
-    })
-  })
+  expect(status).toBe(200)
+  expect(headers).toHaveProperty('x-powered-by', 'msw')
+  expect(new Uint8Array(body)).toEqual(new Uint8Array(expectedBuffer))
 })

cbaedfac5f075b5473a4fae6b3d865144f5320be
Artem Zakharchenko <kettanaito@gmail.com>
2020-07-15 10:24:31 +0200

    Adjusts the integration test for "res.networkError"

--- test/msw-api/res/network-error.test.ts
+++ test/msw-api/res/network-error.test.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 import { TestAPI, runBrowserWith } from '../../support/runBrowserWith'
+import { captureConsole } from '../../support/captureConsole'
 
 let runtime: TestAPI
 
@@ -11,8 +12,25 @@ beforeAll(async () => {
 
 afterAll(() => runtime.cleanup())
 
-test('res.networkError causes Fetch API to throw error', async () => {
-  expect(runtime.request({ url: 'http://test.io/user' })).rejects.toThrow(
-    'Network error',
+test('throws a network error', async () => {
+  const errors: string[] = []
+  captureConsole(runtime.page, errors, (message) => {
+    return message.type() === 'error'
+  })
+
+  // Do not use `runtime.request()`, because it always awaits a response.
+  // In this case we await a network error, performing a request manually.
+  const requestPromise = runtime.page.evaluate(() => {
+    return fetch('/user')
+  })
+
+  await expect(requestPromise).rejects.toThrow(
+    // The `fetch` call itself rejects with the "Failed to fetch" error,
+    // the same error that happens on a regular network error.
+    'Evaluation failed: TypeError: Failed to fetch',
   )
+
+  // Assert a network error message printed into the console
+  // before `fetch` rejects.
+  expect(errors).toContain('Failed to load resource: net::ERR_FAILED')
 })

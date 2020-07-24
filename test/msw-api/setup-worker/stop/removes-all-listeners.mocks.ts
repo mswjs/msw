71081051e2a98afbd9182bc92eaf2daed5ede97d
Artem Zakharchenko <kettanaito@gmail.com>
2020-07-24 11:34:46 +0200

    Restructures the listeners removal test scenario

--- test/msw-api/setup-worker/stop/removes-all-listeners.mocks.ts
+++ test/msw-api/setup-worker/stop/removes-all-listeners.mocks.ts
@@ -1,11 +1,12 @@
 import { setupWorker, rest } from 'msw'
 
-const createWorker = () =>
-  setupWorker(
+const createWorker = () => {
+  return setupWorker(
     rest.get('/user', (req, res, ctx) => {
       return res(ctx.status(200))
     }),
   )
+}
 
 // @ts-ignore
 window.__MSW_CREATE_WORKER__ = createWorker

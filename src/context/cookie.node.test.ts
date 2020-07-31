79db1bf2f5b32869b024bc2c5b918b30558b0660
Eduardo Pinho <eduardopinho@bmd-software.com>
2020-07-31 14:08:46 +0100

    Refactor test: set cookie on response headers node

--- src/context/cookie.node.test.ts
+++ src/context/cookie.node.test.ts
@@ -4,19 +4,9 @@
 import { cookie } from './cookie'
 import { response } from '../response'
 
-describe('cookie (Node.js without jsdom)', () => {
-  describe('given I set a cookie', () => {
-    let result: ReturnType<typeof response>
-
-    beforeAll(() => {
-      result = response(cookie('my-cookie', 'arbitrary-value'))
-      expect(result).toBeDefined()
-    })
-
-    it('should be set on the response headers', () => {
-      expect(result.headers.get('set-cookie')).toEqual(
-        'my-cookie=arbitrary-value',
-      )
-    })
-  })
+test('sets a cookie on the response headers, node environment', () => {
+  const result: ReturnType<typeof response> = response(
+    cookie('my-cookie', 'arbitrary-value'),
+  )
+  expect(result.headers.get('set-cookie')).toEqual('my-cookie=arbitrary-value')
 })

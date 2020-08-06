3f1f7af0b5cdf89a189858015bb8541127ce25c5
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-06 08:33:14 +0200

    Adds test suite for entirely side-effect request handlers

--- test/msw-api/setup-worker/fall-through.mocks.ts
+++ test/msw-api/setup-worker/fall-through.mocks.ts
@@ -5,6 +5,9 @@ const worker = setupWorker(
   rest.get('/us*', () => console.log('[get] second')),
   rest.get('/user', (req, res, ctx) => res(ctx.json({ firstName: 'John' }))),
   rest.get('/user', () => console.log('[get] third')),
+
+  rest.post('/blog/*', () => console.log('[post] one')),
+  rest.post('/blog/article', () => console.log('[post] two')),
 )
 
 worker.start()

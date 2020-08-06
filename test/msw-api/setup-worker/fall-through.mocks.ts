d04c06ac9b007d9149342c8bc25aa7627592c5fa
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-06 07:14:35 +0200

    Renames the "fall-through" test module

--- test/msw-api/setup-worker/fall-through.mocks.ts
+++ test/msw-api/setup-worker/fall-through.mocks.ts
@@ -4,6 +4,7 @@ const worker = setupWorker(
   rest.get('*', () => console.log('[test] first caught')),
   rest.get('/us*', () => console.log('[test] second caught')),
   rest.get('/user', (req, res, ctx) => res(ctx.json({ firstName: 'John' }))),
+  rest.get('/user', () => console.log('[test] third caught')),
 )
 
 worker.start()

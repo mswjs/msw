847ec554646d67eec561b64c0843d340c6ef485c
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-24 09:44:21 +0200

    setupWorker: Adds integration test for custom "serviceWorker.options.scope" value

---
+++ test/msw-api/setup-worker/start/options-sw-scope.test.ts
@@ -0,0 +1,40 @@
+import * as path from 'path'
+import { TestAPI, runBrowserWith } from '../../../support/runBrowserWith'
+import { captureConsole } from '../../../support/captureConsole'
+
+let runtime: TestAPI
+
+beforeAll(async () => {
+  runtime = await runBrowserWith(
+    path.resolve(__dirname, 'options-sw-scope.mocks.ts'),
+  )
+})
+
+afterAll(() => {
+  return runtime.cleanup()
+})
+
+test('respects a custom "scope" Service Worker option', async () => {
+  const { messages } = captureConsole(runtime.page)
+
+  await runtime.page.evaluate(() => {
+    // @ts-ignore
+    return window.__MSW_REGISTRATION__
+  })
+
+  await runtime.reload()
+
+  const activationMessage = messages.startGroupCollapsed.find((text) => {
+    return text.includes('[MSW] Mocking enabled.')
+  })
+  expect(activationMessage).toBeTruthy()
+
+  const res = await runtime.request({
+    url: `${runtime.origin}/user`,
+  })
+  const status = res.status()
+
+  // Since the root "/" page lies outside of the custom worker scope,
+  // it won't be able to intercept an otherwise matching request.
+  expect(status).toBe(404)
+})

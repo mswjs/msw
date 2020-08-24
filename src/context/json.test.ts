78010427f93fb43330734f43274f9b89804b1726
Neil Kistner <neil.kistner@gmail.com>
2020-08-24 14:47:12 -0500

    Add support for more types passed to `context.json` helper
    Close #349

--- src/context/json.test.ts
+++ src/context/json.test.ts
@@ -3,18 +3,28 @@ import { response } from '../response'
 
 describe('json', () => {
   describe('given a JSON body', () => {
-    let result: ReturnType<typeof response>
-
-    beforeAll(() => {
-      result = response(json({ firstName: 'John' }))
-    })
-
     it('should have "Content-Type" as "application/json"', () => {
+      const result = response(json({ firstName: 'John' }))
+
       expect(result.headers.get('content-type')).toEqual('application/json')
     })
 
     it('should have body set to the given JSON', () => {
-      expect(result).toHaveProperty('body', `{"firstName":"John"}`)
+      const object = response(json({ firstName: 'John' }))
+
+      expect(object).toHaveProperty('body', `{"firstName":"John"}`)
+
+      const array = response(json([1, '2', true, { ok: true }, '']))
+
+      expect(array).toHaveProperty('body', '[1,"2",true,{"ok":true},""]')
+
+      const string = response(json('Some string'))
+
+      expect(string).toHaveProperty('body', '"Some string"')
+
+      const bool = response(json(true))
+
+      expect(bool).toHaveProperty('body', 'true')
     })
   })
 })
--- src/context/json.ts
+++ src/context/json.ts
@@ -1,11 +1,17 @@
 import { ResponseTransformer } from '../response'
 
+type JsonArray = (JsonValue | JsonRecord)[]
+type JsonRecord = Record<string, unknown>
+type JsonValue = string | boolean | number | JsonRecord | JsonArray | null
+
 /**
  * Sets the given Object as the JSON body of the response.
  * @example
  * res(json({ foo: 'bar' }))
+ * res(json('Some string'))
+ * res(json([1, '2', false, { ok: true }]))
  */
-export const json = (body: Record<string, any>): ResponseTransformer => {
+export const json = (body: JsonValue): ResponseTransformer => {
   return (res) => {
     res.headers.set('Content-Type', 'application/json')
     res.body = JSON.stringify(body)

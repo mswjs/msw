5f6db406203b53801b7dd861403d02bdfc3a7ca0
Neil Kistner <neil.kistner@gmail.com>
2020-08-25 09:08:20 -0500

    Respond to PR feedback
    - Update comment to reflect usage
    - Update type to match `JSON.stringify`
    - Cleanup tests and add a date assertion

--- src/context/json.ts
+++ src/context/json.ts
@@ -1,17 +1,13 @@
 import { ResponseTransformer } from '../response'
 
-type JsonArray = (JsonValue | JsonRecord)[]
-type JsonRecord = Record<string, unknown>
-type JsonValue = string | boolean | number | JsonRecord | JsonArray | null
-
 /**
- * Sets the given Object as the JSON body of the response.
+ * Sets the given value as the JSON body of the response.
  * @example
  * res(json({ foo: 'bar' }))
  * res(json('Some string'))
  * res(json([1, '2', false, { ok: true }]))
  */
-export const json = (body: JsonValue): ResponseTransformer => {
+export const json = (body: any): ResponseTransformer => {
   return (res) => {
     res.headers.set('Content-Type', 'application/json')
     res.body = JSON.stringify(body)

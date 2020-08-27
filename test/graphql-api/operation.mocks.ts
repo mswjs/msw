3a1ec89870db3cc7a7c9d1ad9873e6c80a9d84b7
Ryan Worth <ryan@edgeonline.com.au>
2020-08-27 11:08:47 +1000

    rename operations api to operation

--- test/graphql-api/operation.mocks.ts
+++ test/graphql-api/operation.mocks.ts
@@ -1,7 +1,7 @@
 import { setupWorker, graphql } from 'msw'
 
 const worker = setupWorker(
-  graphql.operations((req, res, ctx) => {
+  graphql.operation((req, res, ctx) => {
     return res(
       ctx.data({
         query: req.body.query,

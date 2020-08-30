abbd5784573095d5683cae1cc2ab3f273110ab94
Artem Zakharchenko <kettanaito@gmail.com>
2020-08-30 17:49:10 +0200

    deferNetworkRequestsUntil: Changes the explanatory comments slightly

--- src/utils/deferNetworkRequestsUntil.ts
+++ src/utils/deferNetworkRequestsUntil.ts
@@ -6,21 +6,21 @@ import { until } from '@open-draft/until'
  * Must only be used in a browser.
  */
 export function deferNetworkRequestsUntil(predicatePromise: Promise<any>) {
-  // Defer `XMLHttpRequest` until the Service Worker is ready.
+  // Defer any `XMLHttpRequest` requests until the Service Worker is ready.
   const originalXhrSend = window.XMLHttpRequest.prototype.send
-
   window.XMLHttpRequest.prototype.send = function (
     ...args: Parameters<XMLHttpRequest['send']>
   ) {
+    // Keep this function synchronous to comply with `XMLHttpRequest.prototype.send`,
+    // because that method is always synchronous.
     until(() => predicatePromise).then(() => {
       window.XMLHttpRequest.prototype.send = originalXhrSend
       this.send(...args)
     })
   }
 
-  // Defer `fetch` requests until the Service Worker is ready.
+  // Defer any `fetch` requests until the Service Worker is ready.
   const originalFetch = window.fetch
-
   window.fetch = async (...args) => {
     await until(() => predicatePromise)
     window.fetch = originalFetch

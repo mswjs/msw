e73e6e54f88e92871830b2490e7e28b350f3c1b3
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-29 16:03:24 +1000

    Format shared.test.ts

--- test/msw-api/setup-worker/start/shared.test.ts
+++ test/msw-api/setup-worker/start/shared.test.ts
@@ -1,12 +1,11 @@
 import * as path from 'path'
-import { TestAPI, runBrowserWith } from '../../../support/runBrowserWith'
+import { runBrowserWith } from '../../../support/runBrowserWith'
 import { captureConsole } from '../../../support/captureConsole'
 
-
-
-
 test('shares the client registration for all other clients when "shared" option is set to "true"', async () => {
-  const runtime = await runBrowserWith(path.resolve(__dirname, 'shared.mocks.ts'))
+  const runtime = await runBrowserWith(
+    path.resolve(__dirname, 'shared.mocks.ts'),
+  )
   const { messages } = captureConsole(runtime.page)
   const iframeUrl = `${runtime.origin}/test/support/template/iframe.html`
 
@@ -45,6 +44,6 @@ test('shares the client registration for all other clients when "shared" option 
   })
 
   expect(requestsLog).toMatch(/\[MSW\] \d{2}:\d{2}:\d{2} GET \/user 200/)
-  
+
   return runtime.cleanup()
 })

30e5ab1916977ecf718b69cda7858942b3eb8830
Artem Zakharchenko <kettanaito@gmail.com>
2020-10-28 09:30:59 +0100

    ctx.status: Flattens unit tests

--- src/context/status.test.ts
+++ src/context/status.test.ts
@@ -1,36 +1,14 @@
 import { status } from './status'
 import { response } from '../response'
 
-describe('status', () => {
-  describe('given a status code', () => {
-    let result: ReturnType<typeof response>
+test('sets given status code on the response', () => {
+  const result = response(status(403))
+  expect(result).toHaveProperty('status', 403)
+  expect(result).toHaveProperty('statusText', 'Forbidden')
+})
 
-    beforeAll(() => {
-      result = response(status(403))
-    })
-
-    it('should set status code on the response', () => {
-      expect(result).toHaveProperty('status', 403)
-    })
-
-    it('should have the status text associated with the status code', () => {
-      expect(result).toHaveProperty('statusText', 'Forbidden')
-    })
-  })
-
-  describe('given a status code and a status text', () => {
-    let result: ReturnType<typeof response>
-
-    beforeAll(() => {
-      result = response(status(301, 'Custom text'))
-    })
-
-    it('should set status code on the response', () => {
-      expect(result).toHaveProperty('status', 301)
-    })
-
-    it('should have the custom status text', () => {
-      expect(result).toHaveProperty('statusText', 'Custom text')
-    })
-  })
+test('supports custom status text', () => {
+  const result = response(status(301, 'Custom text'))
+  expect(result).toHaveProperty('status', 301)
+  expect(result).toHaveProperty('statusText', 'Custom text')
 })

7429ddbf3a71f43d430d912432f4dbf239b212a2
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-20 15:28:28 +0100

    Renames "rest" unit test suite

--- src/rest.spec.ts
+++ src/rest.spec.ts
@@ -1,7 +1,7 @@
 import { rest } from './rest'
 
-test('Exports all REST API methods', () => {
-  expect(rest).not.toBeUndefined()
+test('exports all REST API methods', () => {
+  expect(rest).toBeDefined()
   expect(Object.keys(rest)).toEqual([
     'head',
     'get',

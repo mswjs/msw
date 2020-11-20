11858c2ee53244ad1bc9aad473007d2ccbee356b
Otacilio Lacerda <otaciliolacerda@gmail.com>
2020-11-20 15:52:46 +0100

    fix: do not stringify res.body if content is string

--- src/context/body.test.ts
+++ src/context/body.test.ts
@@ -1,4 +1,5 @@
 import { body } from './body'
+import { set } from './set'
 import { response } from '../response'
 
 test('sets a given body value without implicit "Content-Type" header', async () => {
@@ -6,4 +7,13 @@ test('sets a given body value without implicit "Content-Type" header', async () 
 
   expect(result).toHaveProperty('body', 'Lorem ipsum')
   expect(result.headers.get('content-type')).toBeNull()
+})
+
+test('does not stringify raw body twice if content is string and "Content-Type" header is "json"', async () => {
+  const result = await response(
+    set('Content-Type', 'application/json'),
+    body(JSON.stringify('some text')),
+  )
+
+  expect(result).toHaveProperty('body', `"some text"`)
 })

5ec66d24082d04e31f8547cd168d960cac61158c
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-27 10:30:00 +0100

    Adds type annotations to the "compose" function

--- src/utils/internal/compose.test.ts
+++ src/utils/internal/compose.test.ts
@@ -2,12 +2,12 @@ import { compose } from './compose'
 
 test('composes given functions from right to left', () => {
   const list: number[] = []
-
   const populateList = compose(
     () => list.push(1),
     () => list.push(7),
     () => list.push(5),
   )
+
   populateList()
 
   expect(list).toEqual([5, 7, 1])
@@ -15,8 +15,8 @@ test('composes given functions from right to left', () => {
 
 test('composes a list of async functions from right to left', async () => {
   const generateNumber = compose(
-    async (n) => n + 1,
-    async (n) => n * 5,
+    async (n: number) => n + 1,
+    async (n: number) => n * 5,
   )
   const number = await generateNumber(5)
 
--- src/utils/internal/compose.ts
+++ src/utils/internal/compose.ts
@@ -1,17 +1,31 @@
+type ArityOneFn = (arg: any) => any
+type PickLastInTuple<T extends any[]> = T extends [
+  ...rest: infer U,
+  argn: infer L,
+]
+  ? L
+  : never
+type FirstFnParameterType<T extends any[]> = Parameters<PickLastInTuple<T>>[any]
+type LastFnParameterType<T extends any[]> = ReturnType<T[0]>
+
 /**
  * Composes a given list of functions into a new function that
  * executes from right to left.
  */
-export function compose(...funcs: Array<(...args: any[]) => any>) {
-  return funcs.reduce((f, g) => {
-    return (...args: any[]) => {
-      const res = g(...args)
-
-      if (res instanceof Promise) {
-        return Promise.resolve(res).then(f)
-      }
-
-      return f(res)
-    }
-  })
+export function compose<
+  T extends ArityOneFn[],
+  LeftReturnType extends FirstFnParameterType<T>,
+  RightReturnType extends LastFnParameterType<T>
+>(
+  ...fns: T
+): (
+  ...args: LeftReturnType extends never ? never[] : [LeftReturnType]
+) => RightReturnType {
+  return (...args) => {
+    return fns.reduceRight((leftFn: any, rightFn) => {
+      return leftFn instanceof Promise
+        ? Promise.resolve(leftFn).then(rightFn)
+        : rightFn(leftFn)
+    }, args[0])
+  }
 }

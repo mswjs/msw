64006fce9c04acdf839849bcf3044225f3bac61e
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-30 12:34:47 +0100

    data, errors: Removes JSON merging

--- src/context/data.ts
+++ src/context/data.ts
@@ -7,5 +7,5 @@ export type DataContext<T> = (payload: T) => ResponseTransformer
  * Returns a GraphQL body payload.
  */
 export const data: DataContext<Record<string, any>> = (payload) => {
-  return json({ data: payload }, { merge: true })
+  return json({ data: payload })
 }

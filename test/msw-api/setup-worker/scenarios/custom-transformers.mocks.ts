638e700d1d0af12c4d3ef797bf3976224a1c59a5
Artem Zakharchenko <kettanaito@gmail.com>
2020-11-30 13:01:32 +0100

    Uses custom context util for "setupWorker" custom transformers integration test

--- test/msw-api/setup-worker/scenarios/custom-transformers.mocks.ts
+++ test/msw-api/setup-worker/scenarios/custom-transformers.mocks.ts
@@ -1,24 +1,21 @@
-import { setupWorker, rest, createResponseComposition } from 'msw'
+import { ResponseTransformer, compose, context, setupWorker, rest } from 'msw'
 import * as JSONbig from 'json-bigint'
 
-const customReponse = createResponseComposition(null, [
-  (res) => {
-    if (res.body && res.headers?.get('content-type')?.includes('json')) {
-      res.body = JSONbig.stringify(res.body)
-    }
-
-    return res
-  },
-])
+const jsonBig = (body: Record<string, any>): ResponseTransformer => {
+  return compose(
+    context.set('Content-Type', 'application/json'),
+    context.body(JSONbig.stringify(body)),
+  )
+}
 
 const worker = setupWorker(
   rest.get('/user', (req, res, ctx) => {
-    const user = {
-      username: 'john.maverick',
-      balance: BigInt(1597928668063727616),
-    }
-
-    return customReponse(ctx.json(user))
+    return res(
+      jsonBig({
+        username: 'john.maverick',
+        balance: BigInt(1597928668063727616),
+      }),
+    )
   }),
 )
 

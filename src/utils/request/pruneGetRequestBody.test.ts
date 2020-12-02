c07f3944250ac01ee6d0d0f6ccfac259963f8830
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-02 15:42:21 +0100

    Normalizes already parsed Service Worker request

---
+++ src/utils/request/pruneGetRequestBody.test.ts
@@ -0,0 +1,35 @@
+import { pruneGetRequestBody } from './pruneGetRequestBody'
+
+test('sets empty GET request body to undefined', () => {
+  const body = pruneGetRequestBody({
+    method: 'GET',
+    body: '',
+  })
+
+  expect(body).toBeUndefined()
+})
+
+test('preserves non-empty GET request body', () => {
+  const body = pruneGetRequestBody({
+    method: 'GET',
+    body: 'text-body',
+  })
+
+  expect(body).toBe('text-body')
+})
+
+test('ignores requests of the other method than GET', () => {
+  expect(
+    pruneGetRequestBody({
+      method: 'HEAD',
+      body: JSON.stringify({ a: 1 }),
+    }),
+  ).toBe(JSON.stringify({ a: 1 }))
+
+  expect(
+    pruneGetRequestBody({
+      method: 'POST',
+      body: 'text-body',
+    }),
+  ).toBe('text-body')
+})
--- src/utils/worker/handleRequestWith.ts
+++ src/utils/worker/handleRequestWith.ts
@@ -1,71 +1,34 @@
-import { Headers, headersToList } from 'headers-utils'
+import { headersToList } from 'headers-utils'
 import {
   StartOptions,
   ResponseWithSerializedHeaders,
   SetupWorkerInternalContext,
+  ServiceWorkerIncomingEventsMap,
 } from '../../setupWorker/glossary'
-import { MockedRequest } from '../handlers/requestHandler'
 import {
   ServiceWorkerMessage,
   createBroadcastChannel,
 } from '../createBroadcastChannel'
 import { getResponse } from '../getResponse'
 import { onUnhandledRequest } from '../request/onUnhandledRequest'
-import { parseBody } from '../request/parseBody'
-import { getRequestCookies } from '../request/getRequestCookies'
-import { isStringEqual } from '../internal/isStringEqual'
 import { NetworkError } from '../NetworkError'
+import { parseWorkerRequest } from '../request/parseWorkerRequest'
 
 export const handleRequestWith = (
   context: SetupWorkerInternalContext,
   options: StartOptions,
 ) => {
-  return async (event: MessageEvent) => {
+  return async (
+    event: MessageEvent,
+    message: ServiceWorkerMessage<
+      'REQUEST',
+      ServiceWorkerIncomingEventsMap['REQUEST']
+    >,
+  ) => {
     const channel = createBroadcastChannel(event)
 
     try {
-      const message: ServiceWorkerMessage<
-        'REQUEST',
-        MockedRequest
-      > = JSON.parse(event.data, function (this: MockedRequest, key, value) {
-        if (key === 'url') {
-          return new URL(value)
-        }
-
-        // Serialize headers
-        if (key === 'headers') {
-          return new Headers(value)
-        }
-
-        // Prevent empty fields from presering an empty value.
-        // It's invalid to perform a GET request with { body: "" }
-        if (
-          // Check if we are parsing deeper in `event.data.payload`,
-          // because this custom JSON parser is invoked for each depth level.
-          this.method &&
-          isStringEqual(this.method, 'GET') &&
-          key === 'body' &&
-          value === ''
-        ) {
-          return undefined
-        }
-
-        return value
-      })
-
-      const { type, payload: req } = message
-
-      // Ignore irrelevant worker message types
-      if (type !== 'REQUEST') {
-        return null
-      }
-
-      // Parse the request's body based on the "Content-Type" header.
-      req.body = parseBody(req.body, req.headers)
-
-      // Set document cookies on the request.
-      req.cookies = getRequestCookies(req)
-
+      const req = parseWorkerRequest(message.payload)
       context.emitter.emit('request:start', req)
 
       const {

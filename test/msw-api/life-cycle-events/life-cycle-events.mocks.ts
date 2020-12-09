3e16580c432c1b8ab76618b95f818e2f1384a8fe
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-09 10:13:15 +0100

    Adds integration tests for the life-cycle API events

---
+++ test/msw-api/life-cycle-events/life-cycle-events.mocks.ts
@@ -0,0 +1,39 @@
+import { rest, setupWorker } from 'msw'
+
+const worker = setupWorker(
+  rest.get('/user', (req, res, ctx) => {
+    return res(ctx.text('response-body'))
+  }),
+  rest.post('/no-response', (req, res, ctx) => {
+    return
+  }),
+)
+
+worker.on('request:start', (req) => {
+  console.warn(`[request:start] ${req.method} ${req.url.href} ${req.id}`)
+})
+
+worker.on('request:match', (req) => {
+  console.warn(`[request:match] ${req.method} ${req.url.href} ${req.id}`)
+})
+
+worker.on('request:unhandled', (req) => {
+  console.warn(`[request:unhandled] ${req.method} ${req.url.href} ${req.id}`)
+})
+
+worker.on('request:end', (req) => {
+  console.warn(`[request:end] ${req.method} ${req.url.href} ${req.id}`)
+})
+
+worker.on('response:mocked', async (res, reqId) => {
+  const body = await res.text()
+  console.warn(`[response:mocked] ${body} ${reqId}`)
+})
+
+worker.on('response:bypass', (res, reqId) => {
+  console.warn(`[response:bypass] ${reqId}`)
+})
+
+worker.start()
+// @ts-ignore
+window.worker = worker

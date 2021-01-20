adec010ebab0faf8a58d59f06f833af9f95950dd
Artem Zakharchenko <kettanaito@gmail.com>
2021-01-20 14:18:51 +0100

    Adds a browser integration test for GraphQL response patching

---
+++ test/graphql-api/response-patching.mocks.ts
@@ -0,0 +1,42 @@
+import { setupWorker, graphql } from 'msw'
+import { getClient, gql } from '../support/graphql'
+
+const worker = setupWorker(
+  graphql.query('GetUser', async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalJson = await originalResponse.json()
+
+    return res(
+      ctx.data({
+        user: {
+          firstName: 'Christian',
+          lastName: originalJson.data?.user?.lastName,
+        },
+      }),
+      ctx.errors(originalJson.errors),
+    )
+  }),
+)
+
+const registration = worker.start()
+
+// @ts-ignore
+window.msw = {
+  registration,
+}
+
+// @ts-ignore
+window.dispatchGraphQLQUery = (uri: string) => {
+  const client = getClient({ uri })
+
+  return client.query({
+    query: gql`
+      query GetUser {
+        user {
+          firstName
+          lastName
+        }
+      }
+    `,
+  })
+}
--- test/graphql-api/response-patching.test.ts
+++ test/graphql-api/response-patching.test.ts
@@ -1,51 +1,39 @@
-import { graphql } from 'msw'
-import { setupServer } from 'msw/node'
-import { graphql as executeGraphql } from 'graphql'
-import { buildSchema } from 'graphql/utilities'
-import { createApolloFetch } from 'apollo-fetch'
+import * as path from 'path'
+import { buildSchema, graphql } from 'graphql'
+import { ApolloQueryResult } from '@apollo/client'
 import { ServerApi, createServer } from '@open-draft/test-server'
+import { SetupWorkerApi } from 'msw'
+import { runBrowserWith } from '../support/runBrowserWith'
+
+declare namespace window {
+  export const dispatchGraphQLQUery: (
+    uri: string,
+  ) => Promise<ApolloQueryResult<any>>
+
+  export const msw: {
+    registration: SetupWorkerApi['start']
+  }
+}
 
 let prodServer: ServerApi
 
-const server = setupServer(
-  graphql.query('GetUser', async (req, res, ctx) => {
-    const originalResponse = await ctx.fetch(req)
-    const { requestHeaders, queryResult } = await originalResponse.json()
-
-    return res(
-      ctx.data({
-        user: {
-          firstName: 'Christian',
-          lastName: queryResult.data?.user?.lastName,
-        },
-        // Setting the request headers on the response data on purpose
-        // to access them in the response of the Apollo client.
-        requestHeaders,
-      }),
-      ctx.errors(queryResult.errors),
-    )
-  }),
-)
-
 beforeAll(async () => {
-  server.listen()
-
-  // This test server acts as a production server MSW will be hitting
-  // when performing a request patching with `ctx.fetch()`.
+  // This test server simulates a production GraphQL server
+  // and uses a hard-coded `rootValue` to resolve queries
+  // against the schema.
   prodServer = await createServer((app) => {
     app.post('/graphql', async (req, res) => {
-      const result = await executeGraphql({
+      const result = await graphql({
         schema: buildSchema(`
-          type User {
-            firstName: String!
-            lastName: String!
-          }
+            type User {
+              firstName: String!
+              lastName: String!
+            }
 
-          type Query {
-            user: User!
-          }
-        `),
-        operationName: 'GetUser',
+            type Query {
+              user: User!
+            }
+          `),
         source: req.body.query,
         rootValue: {
           user: {
@@ -55,40 +43,39 @@ beforeAll(async () => {
         },
       })
 
-      return res.status(200).json({
-        requestHeaders: req.headers,
-        queryResult: result,
-      })
+      return res.status(200).json(result)
     })
   })
 })
 
 afterAll(async () => {
-  server.close()
   await prodServer.close()
 })
 
+function createRuntime() {
+  return runBrowserWith(path.join(__dirname, 'response-patching.mocks.ts'))
+}
+
 test('patches a GraphQL response', async () => {
-  const fetch = createApolloFetch({
-    uri: prodServer.http.makeUrl('/graphql'),
+  const runtime = await createRuntime()
+  const prodServerUrl = prodServer.http.makeUrl('/graphql')
+
+  await runtime.page.evaluate(() => {
+    return window.msw.registration
   })
 
-  const res = await fetch({
-    query: `
-      query GetUser {
-        user {
-          firstName
-          lastName
-        }
-      }
-    `,
-  })
+  const res = await runtime.page.evaluate(
+    (url) => {
+      return window.dispatchGraphQLQUery(url)
+    },
+    [prodServerUrl],
+  )
 
   expect(res.errors).toBeUndefined()
   expect(res.data).toHaveProperty('user', {
     firstName: 'Christian',
     lastName: 'Maverick',
   })
-  expect(res.data.requestHeaders).toHaveProperty('x-msw-bypass', 'true')
-  expect(res.data.requestHeaders).not.toHaveProperty('map')
+
+  return runtime.cleanup()
 })

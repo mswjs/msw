c14d9ee0f8b4f259c25f6bda7fa0ea8aa7d4fa70
Artem Zakharchenko <kettanaito@gmail.com>
2021-01-24 10:23:59 +0100

    Awaits a CORS resource "load" event instead of "sleep"

--- test/rest-api/cors.mocks.ts
+++ test/rest-api/cors.mocks.ts
@@ -1,5 +1,12 @@
-import { setupWorker, rest } from 'msw'
+import { setupWorker } from 'msw'
 
 const worker = setupWorker()
 
 worker.start()
+
+worker.on('response:mocked', (res) => {
+  console.log('[mocked]', res)
+})
+worker.on('response:bypass', (res) => {
+  console.log('[bypassed]', res)
+})

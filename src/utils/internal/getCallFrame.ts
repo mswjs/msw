f7bf35952edfa2f54771a0aa941d3656ccc4e100
Peter Hozak <peter.hozak@gmail.com>
2021-01-29 23:39:03 +0100

    fix: getCallFrame does not need to throw #562

--- src/utils/internal/getCallFrame.ts
+++ src/utils/internal/getCallFrame.ts
@@ -2,25 +2,19 @@
  * Return the stack trace frame of a function's invocation.
  */
 export function getCallFrame() {
-  try {
-    const inspectionError = new Error()
-    inspectionError.name = 'Inspection Error'
-    throw inspectionError
-  } catch (error) {
-    const frames: string[] = error.stack.split('\n')
+  const frames: string[] = (new Error().stack as string).split('\n')
 
-    // Get the first frame that doesn't reference the library's internal trace.
-    // Assume that frame is the invocation frame.
-    const declarationFrame = frames.slice(1).find((frame) => {
-      return !/(node_modules)?\/lib\/(umd|esm)\//.test(frame)
-    })
+  // Get the first frame that doesn't reference the library's internal trace.
+  // Assume that frame is the invocation frame.
+  const declarationFrame = frames.slice(1).find((frame) => {
+    return !/(node_modules)?\/lib\/(umd|esm)\//.test(frame)
+  })
 
-    if (!declarationFrame) {
-      return
-    }
-
-    // Extract file reference from the stack frame.
-    const [, declarationPath] = declarationFrame.match(/\((.+?)\)$/) || []
-    return declarationPath
+  if (!declarationFrame) {
+    return
   }
+
+  // Extract file reference from the stack frame.
+  const [, declarationPath] = declarationFrame.match(/\((.+?)\)$/) || []
+  return declarationPath
 }

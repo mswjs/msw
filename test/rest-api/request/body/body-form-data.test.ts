cee5f80c363482b7df6fdb58d14dbfa0ff5445a7
Artem Zakharchenko <kettanaito@gmail.com>
2021-02-15 17:14:59 +0100

    Adds FormData request body integration test

---
+++ test/rest-api/request/body/body-form-data.test.ts
@@ -0,0 +1,21 @@
+import * as path from 'path'
+import { pageWith } from 'page-with'
+
+test('handles FormData as a request body', async () => {
+  const { page, makeUrl, debug } = await pageWith({
+    example: path.resolve(__dirname, 'body-form-data.mocks.ts'),
+    template: path.resolve(__dirname, 'body-form-data.page.html'),
+  })
+
+  await page.click('button')
+
+  const res = await page.waitForResponse(makeUrl('/sign-in'))
+  const status = res.status()
+  const json = await res.json()
+
+  expect(status).toBe(200)
+  expect(json).toEqual({
+    username: 'john.maverick',
+    password: 'secret123',
+  })
+})

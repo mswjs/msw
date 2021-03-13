12bdd274c2e283bdd16448813e58b510ec676ffc
Artem Zakharchenko <kettanaito@gmail.com>
2021-03-13 13:02:49 +0100

    Adds integration test for a sibling iframe request interception

--- test/msw-api/setup-worker/scenarios/iframe/iframe.test.ts
+++ test/msw-api/setup-worker/scenarios/iframe/iframe.test.ts
@@ -68,3 +68,45 @@ test('intercepts a request from a deeply nested iframe given MSW is registered i
 
   expect(firstName).toBe('John')
 })
+
+test('intercepts a request from an iframe given MSW is registered in a sibling iframe', async () => {
+  // A request-issuing frame. Here lives the `window.fetch` call.
+  const requestFrame = await pageWith({
+    title: 'Request frame',
+    example: null,
+    markup: path.resolve(__dirname, 'page-in-iframe.html'),
+    contentBase: path.resolve(__dirname),
+  })
+
+  // A frame that registers MSW, but does no requests.
+  const mswFrame = await pageWith({
+    title: 'MSW frame',
+    example: path.resolve(__dirname, 'iframe.mocks.ts'),
+    contentBase: path.resolve(__dirname),
+  })
+
+  // A parent frame that hosts two frames above.
+  const parentPage = await pageWith({
+    title: 'Parent page',
+    example: null,
+    markup: `
+<iframe src="${requestFrame.page.url()}"></iframe>
+<iframe src="${mswFrame.page.url()}"></iframe>
+    `,
+  })
+
+  await parentPage.page.bringToFront()
+
+  const frame = parentPage.page
+    .mainFrame()
+    .childFrames()
+    .find(findFrame)
+    .childFrames()
+    .find(findFrame)
+
+  await frame.evaluate(() => window.request())
+  const firstNameElement = await frame.waitForSelector('#first-name')
+  const firstName = await firstNameElement.evaluate((node) => node.textContent)
+
+  expect(firstName).toBe('John')
+})

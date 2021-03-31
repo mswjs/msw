378248c998bc40451a2a3fc9165002722d3b8955
Matthew Leon <ml@matthewleon.com>
2021-03-31 18:39:59 -0400

    review: add test for ugly content-type header

--- src/utils/internal/parseMultipartData.test.ts
+++ src/utils/internal/parseMultipartData.test.ts
@@ -1,6 +1,24 @@
 import { parseMultipartData } from './parseMultipartData'
 
 test('parses a given valid multipart string', async () => {
+  expect.assertions(3)
+  await testMultipartDataWithContentType(
+    'multipart/form-data; boundary=WebKitFormBoundaryvZ1cVXWyK0ilQdab',
+  )
+})
+
+test('parses a given valid multipart string give non-pretty content-type', async () => {
+  expect.assertions(3)
+  // node-fetch will serialize content-type in this format, which is valid according to HTTP
+  // https://github.com/node-fetch/node-fetch/blob/d8fc32d6b29bd43d1ad377e80b3e439fe37f2904/test/main.js#L1438
+  await testMultipartDataWithContentType(
+    'multipart/form-data;boundary=WebKitFormBoundaryvZ1cVXWyK0ilQdab',
+  )
+})
+
+async function testMultipartDataWithContentType(
+  contentType: string,
+): Promise<void> {
   const body = `\
 ------WebKitFormBoundaryvZ1cVXWyK0ilQdab\r
 Content-Disposition: form-data; name="file"; filename="file1.txt"\r
@@ -23,8 +41,7 @@ another text content 2\r
 \r
 ------WebKitFormBoundaryvZ1cVXWyK0ilQdab--`
   const headers = new Headers({
-    'content-type':
-      'multipart/form-data; boundary=WebKitFormBoundaryvZ1cVXWyK0ilQdab',
+    'content-type': contentType,
   })
   const parsed = parseMultipartData(body, headers)
 
@@ -37,7 +54,7 @@ another text content 2\r
     'another text content',
     '\r\nanother text content 2\r\n',
   ])
-})
+}
 
 test('returns undefined without an error given an invalid multipart string', () => {
   const headers = new Headers({

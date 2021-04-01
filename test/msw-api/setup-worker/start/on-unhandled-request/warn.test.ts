6893e8ff26ffea3b4c2ef53bcec17271e87e405c
Artem Zakharchenko <kettanaito@gmail.com>
2021-04-01 12:05:51 +0200

    Adds integration tests for explicit/implicit short-circuiting in setupWorker handler

--- test/msw-api/setup-worker/start/on-unhandled-request/warn.test.ts
+++ test/msw-api/setup-worker/start/on-unhandled-request/warn.test.ts
@@ -9,7 +9,6 @@ function createRuntime() {
 
 test('warns on an unhandled REST API request with an absolute URL', async () => {
   const { request, consoleSpy } = await createRuntime()
-
   const res = await request('https://mswjs.io/non-existing-page')
   const status = res.status()
 
@@ -26,7 +25,6 @@ test('warns on an unhandled REST API request with an absolute URL', async () => 
 
 test('warns on an unhandled REST API request with a relative URL', async () => {
   const { request, consoleSpy } = await createRuntime()
-
   const res = await request('/user-details')
   const status = res.status()
 
@@ -39,4 +37,36 @@ test('warns on an unhandled REST API request with a relative URL', async () => {
   })
 
   expect(unhandledRequestWarning).toBeDefined()
+})
+
+test('does not warn on request which handler explicitly returns no mocked response', async () => {
+  const { request, consoleSpy } = await createRuntime()
+  const res = await request('/explicit-return', { method: 'POST' })
+  const status = res.status()
+
+  expect(status).toBe(404)
+
+  const unhandledRequestWarning = consoleSpy.get('warning').find((text) => {
+    return /\[MSW\] Warning: captured a request without a matching request handler/.test(
+      text,
+    )
+  })
+
+  expect(unhandledRequestWarning).not.toBeDefined()
+})
+
+test('does not warn on request which handler implicitly returns no mocked response', async () => {
+  const { request, consoleSpy } = await createRuntime()
+  const res = await request('/implicit-return', { method: 'POST' })
+  const status = res.status()
+
+  expect(status).toBe(404)
+
+  const unhandledRequestWarning = consoleSpy.get('warning').find((text) => {
+    return /\[MSW\] Warning: captured a request without a matching request handler/.test(
+      text,
+    )
+  })
+
+  expect(unhandledRequestWarning).not.toBeDefined()
 })

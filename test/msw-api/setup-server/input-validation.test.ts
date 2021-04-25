7a7a45cb3a9b72c21a1f9c475ad69ce63bcba9c9
SoonWon Kwon <swkwon333@gmail.com>
2021-04-25 09:52:05 +0900

    Update input-validation.test.ts

--- test/msw-api/setup-server/input-validation.test.ts
+++ test/msw-api/setup-server/input-validation.test.ts
@@ -4,7 +4,7 @@ import { setupServer } from 'msw/node'
 test('throws an error given an Array of request handlers to setupServer', async () => {
   const createServer = () => {
     // The next line will be ignored because we want to test that an Error
-    // should be trown when `setupServer` parameters are not valid
+    // should be thrown when `setupServer` parameters are not valid
     // @ts-ignore
     return setupServer([
       rest.get('https://test.mswjs.io/book/:bookId', (req, res, ctx) => {

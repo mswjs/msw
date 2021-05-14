e91cb9d9d454dd531faeae7d3332c179605bf301
Artem Zakharchenko <kettanaito@gmail.com>
2021-05-14 16:59:53 +0200

    Adds integration tests for all bundle formats

---
+++ test/msw-api/distribution/esm.mocks.ts
@@ -0,0 +1,9 @@
+import { setupWorker, rest } from 'msw/lib/esm'
+
+const worker = setupWorker(
+  rest.get('/user', (req, res, ctx) => {
+    return res(ctx.json({ firstName: 'John' }))
+  }),
+)
+
+worker.start()
---
+++ test/msw-api/distribution/esm.test.ts
@@ -0,0 +1,18 @@
+import * as path from 'path'
+import { pageWith } from 'page-with'
+
+it('supports the import of the ESM bundle in a browser', async () => {
+  const runtime = await pageWith({
+    example: path.resolve(__dirname, 'esm.mocks.ts'),
+  })
+
+  expect(runtime.consoleSpy.get('error')).toBeUndefined()
+
+  const response = await runtime.request('/user')
+
+  expect(response.status()).toEqual(200)
+  expect(response.headers()).toHaveProperty('x-powered-by', 'msw')
+  expect(await response.json()).toEqual({
+    firstName: 'John',
+  })
+})

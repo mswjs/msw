cbcdeb5834ab2d3930a1f3382623dc2b5d317e7c
Artem Zakharchenko <kettanaito@gmail.com>
2021-06-01 14:40:12 +0200

    RequestHandler: Abstracts resolver generator handling, adds Node.js tests

--- test/rest-api/generator.mocks.ts
+++ test/rest-api/generator.mocks.ts
@@ -1,13 +1,12 @@
 import { setupWorker, rest } from 'msw'
 
 const worker = setupWorker(
-  rest.get('/polling/:count', function* (req, res, ctx) {
-    const { count: maxCount } = req.params
+  rest.get('/polling/:maxCount', function* (req, res, ctx) {
+    const { maxCount } = req.params
     let count = 0
 
     while (count < maxCount) {
       count += 1
-
       yield res(
         ctx.json({
           status: 'pending',
@@ -24,12 +23,28 @@ const worker = setupWorker(
     )
   }),
 
-  rest.get('/polling/:count', (req, res, ctx) => {
-    return res(
+  rest.get('/polling/once/:maxCount', function* (req, res, ctx) {
+    let count = 0
+
+    while (count < req.params.maxCount) {
+      count += 1
+      yield res(
+        ctx.json({
+          status: 'pending',
+          count,
+        }),
+      )
+    }
+
+    return res.once(
       ctx.json({
-        status: 'done',
+        status: 'complete',
+        count,
       }),
     )
+  }),
+  rest.get('/polling/once/:maxCount', (req, res, ctx) => {
+    return res(ctx.json({ status: 'done' }))
   }),
 )
 

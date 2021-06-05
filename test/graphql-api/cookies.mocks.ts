b3c28499ac0110f68c2cd0ff607be4fe0b771f91
Artem Zakharchenko <kettanaito@gmail.com>
2021-06-05 13:35:24 +0200

    GraphQL: Uses "GetUser" query for the cookies example

--- test/graphql-api/cookies.mocks.ts
+++ test/graphql-api/cookies.mocks.ts
@@ -1,11 +1,11 @@
 import { setupWorker, graphql } from 'msw'
 
 const worker = setupWorker(
-  graphql.query('me', (req, res, ctx) => {
+  graphql.query('GetUser', (req, res, ctx) => {
     return res(
       ctx.cookie('test-cookie', 'value'),
       ctx.data({
-        id: '00000000-0000-0000-0000-000000000000',
+        firstName: 'John',
       }),
     )
   }),

6261a3c46d61bd2f2f5b0d13fddc422a0bf6842c
Artem Zakharchenko <kettanaito@gmail.com>
2021-07-19 13:45:07 +0200

    set: Type-validates the forbidden "cookie"/"set-cookie" response headers

---
+++ test/typings/set.test-d.ts
@@ -0,0 +1,41 @@
+import { set } from '../../src/context/set'
+
+set('header', 'value')
+set({
+  one: 'value',
+  two: 'value',
+})
+
+// @ts-expect-error Forbidden response header.
+set('cookie', 'secret')
+// @ts-expect-error Forbidden response header.
+set('Cookie', 'secret')
+// @ts-expect-error Forbidden response header.
+set('cookie2', 'secret')
+// @ts-expect-error Forbidden response header.
+set('Cookie2', 'secret')
+// @ts-expect-error Forbidden response header.
+set('set-cookie', 'secret')
+// @ts-expect-error Forbidden response header.
+set('Set-Cookie', 'secret')
+// @ts-expect-error Forbidden response header.
+set('set-cookie2', 'secret')
+// @ts-expect-error Forbidden response header.
+set('Set-Cookie2', 'secret')
+
+// @ts-expect-error Forbidden response header.
+set({ cookie: 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ Cookie: 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ cookie2: 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ Cookie2: 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ 'set-cookie': 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ 'Set-Cookie': 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ 'set-cookie2': 'secret' })
+// @ts-expect-error Forbidden response header.
+set({ 'Set-Cookie2': 'secret' })

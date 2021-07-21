048f226232cef91df033f250b7248f6416ad42b5
Artem Zakharchenko <kettanaito@gmail.com>
2021-07-21 13:40:40 +0200

    Includes custom "DeepRequired" type in the build

--- global.d.ts
+++ global.d.ts
@@ -1,14 +1,1 @@
 declare const SERVICE_WORKER_CHECKSUM: string
-
-type Fn = (...arg: any[]) => any
-
-type DeepRequired<
-  T,
-  U extends Record<string, any> | Fn | undefined = undefined
-> = T extends Record<string, any>
-  ? {
-      [P in keyof T]-?: NonNullable<T[P]> extends NonNullable<U | Fn>
-        ? NonNullable<T[P]>
-        : DeepRequired<NonNullable<T[P]>, U>
-    }
-  : T

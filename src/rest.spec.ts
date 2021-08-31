172bc104a3f42dd779128fead34fe7ebb612dd87
Artem Zakharchenko <kettanaito@gmail.com>
2021-08-31 13:58:42 +0200

    rest: Adds a new ".all()" API

--- src/rest.spec.ts
+++ src/rest.spec.ts
@@ -3,6 +3,7 @@ import { rest } from './rest'
 test('exports all REST API methods', () => {
   expect(rest).toBeDefined()
   expect(Object.keys(rest)).toEqual([
+    'all',
     'head',
     'get',
     'post',

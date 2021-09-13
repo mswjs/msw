5558ef24b2b20876fd23a2b244bea0e53475a709
Artem Zakharchenko <kettanaito@gmail.com>
2021-09-13 13:55:03 +0200

    test: add integration tests for GraphQL unnamed operation warnings

--- test/graphql-api/query.test.ts
+++ test/graphql-api/query.test.ts
@@ -1,6 +1,9 @@
 import * as path from 'path'
 import { pageWith } from 'page-with'
-import { executeGraphQLQuery } from './utils/executeGraphQLQuery'
+import {
+  GRAPHQL_TEST_URL,
+  executeGraphQLQuery,
+} from './utils/executeGraphQLQuery'
 
 function createRuntime() {
   return pageWith({
@@ -69,4 +72,35 @@ test('mocks a GraphQL query issued with a POST request', async () => {
       },
     },
   })
+})
+
+test('prints a warning when captured an unnamed GraphQL query', async () => {
+  const runtime = await createRuntime()
+
+  const res = await executeGraphQLQuery(runtime.page, {
+    query: `
+      query {
+        user {
+          firstName
+        }
+      }
+    `,
+  })
+
+  expect(runtime.consoleSpy.get('warning')).toEqual(
+    expect.arrayContaining([
+      expect.stringContaining(
+        `\
+[MSW] Failed to intercept a GraphQL request at "POST ${GRAPHQL_TEST_URL}": unnamed GraphQL operations are not supported.
+
+Consider naming this operation or using "graphql.operation" request handler to intercept GraphQL requests regardless of their operation name/type. Read more: https://mswjs.io/docs/api/graphql/operation\
+`,
+      ),
+    ]),
+  )
+
+  expect(res.status).toBeUndefined()
+  expect(runtime.consoleSpy.get('error')).toEqual([
+    'Failed to load resource: net::ERR_FAILED',
+  ])
 })

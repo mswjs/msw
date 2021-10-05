68ada323f8b3acba947297c9be9eb92d7c1a1bb7
Artem Zakharchenko <kettanaito@gmail.com>
2021-10-05 11:39:16 +0200

    test(body-binary): add the original binary response case

--- test/rest-api/response/body/body-binary.test.ts
+++ test/rest-api/response/body/body-binary.test.ts
@@ -1,22 +1,55 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { pageWith } from 'page-with'
+import { createServer, ServerApi } from '@open-draft/test-server'
 
-test('responds with a given binary body', async () => {
-  const runtime = await pageWith({
+const imageBuffer = fs.readFileSync(
+  path.resolve(__dirname, '../../../fixtures/image.jpg'),
+)
+
+let httpServer: ServerApi
+
+function createRuntime() {
+  return pageWith({
     example: path.resolve(__dirname, 'body-binary.mocks.ts'),
   })
+}
+
+beforeAll(async () => {
+  httpServer = await createServer((app) => {
+    app.get('/image', (req, res) => {
+      res.type('jpg')
+      res.end(imageBuffer, 'binary')
+    })
+  })
+})
+
+afterAll(async () => {
+  await httpServer.close()
+})
+
+test('responds with a mocked binary response body', async () => {
+  const runtime = await createRuntime()
 
   const res = await runtime.request('/images/abc-123')
   const status = res.status()
   const headers = await res.allHeaders()
   const body = await res.body()
 
-  const expectedBuffer = fs.readFileSync(
-    path.resolve(__dirname, '../../../fixtures/image.jpg'),
-  )
-
   expect(status).toBe(200)
   expect(headers).toHaveProperty('x-powered-by', 'msw')
-  expect(new Uint8Array(body)).toEqual(new Uint8Array(expectedBuffer))
+  expect(new Uint8Array(body)).toEqual(new Uint8Array(imageBuffer))
+})
+
+test('responds with an original binary response body', async () => {
+  const runtime = await createRuntime()
+
+  const res = await runtime.request(httpServer.http.makeUrl('/image'))
+  const status = res.status()
+  const headers = res.headers()
+  const body = await res.body()
+
+  expect(status).toEqual(200)
+  expect(headers).not.toHaveProperty('x-powered-by', 'msw')
+  expect(new Uint8Array(body)).toEqual(new Uint8Array(imageBuffer))
 })

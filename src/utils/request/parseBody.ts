7b14868ba1eff5cf9664b5e843a2a5d9f9f6ac93
Artem Zakharchenko <kettanaito@gmail.com>
2021-11-12 13:55:04 +0100

    chore(parseBody): use default "contentType" value for strict boolean checks

--- src/utils/request/parseBody.ts
+++ src/utils/request/parseBody.ts
@@ -3,7 +3,7 @@ import { jsonParse } from '../internal/jsonParse'
 import { parseMultipartData } from '../internal/parseMultipartData'
 
 /**
- * Parses a given request/response body based on the `Content-Type` header.
+ * Parses a given request/response body based on the "Content-Type" header.
  */
 export function parseBody(body?: MockedRequest['body'], headers?: Headers) {
   // Return whatever falsey body value is given.
@@ -11,18 +11,18 @@ export function parseBody(body?: MockedRequest['body'], headers?: Headers) {
     return body
   }
 
-  const contentType = headers?.get('content-type')
+  const contentType = headers?.get('content-type') || ''
 
   // If the body has a Multipart Content-Type
   // parse it into an object.
-  const hasMultipartContent = contentType?.startsWith('multipart/form-data')
+  const hasMultipartContent = contentType.startsWith('multipart/form-data')
   if (hasMultipartContent && typeof body !== 'object') {
     return parseMultipartData(body.toString(), headers) || body
   }
 
   // If the intercepted request's body has a JSON Content-Type
   // parse it into an object.
-  const hasJsonContent = contentType?.includes('json')
+  const hasJsonContent = contentType.includes('json')
 
   if (hasJsonContent && typeof body !== 'object') {
     return jsonParse(body.toString()) || body

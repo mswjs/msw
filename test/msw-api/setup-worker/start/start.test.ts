f6e709c3c7d1ac60f99cac6699e83565d8401cd0
Artem Zakharchenko <kettanaito@gmail.com>
2022-03-07 00:48:01 +0100

    fix(setupWorker): resolve the "start" promise after the worker has activated (#1146)

--- test/msw-api/setup-worker/start/start.test.ts
+++ test/msw-api/setup-worker/start/start.test.ts
@@ -1,34 +1,59 @@
 import * as path from 'path'
 import { pageWith } from 'page-with'
 import { SetupWorkerApi } from 'msw'
+import { waitFor } from '../../../support/waitFor'
 
 declare namespace window {
   export const msw: {
-    registration: ReturnType<SetupWorkerApi['start']>
+    startWorker(): ReturnType<SetupWorkerApi['start']>
   }
 }
 
-test('resolves the "start" Promise after the worker has been activated', async () => {
-  const { page, consoleSpy } = await pageWith({
+test('resolves the "start" Promise when the worker has been activated', async () => {
+  const runtime = await pageWith({
     example: path.resolve(__dirname, 'start.mocks.ts'),
-  })
-  const resolvedPayload = await page.evaluate(() => {
-    return window.msw.registration
+    routes(app) {
+      app.get('/worker.js', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'start.worker.js'))
+      })
+    },
   })
 
-  expect(resolvedPayload).toBe('ServiceWorkerRegistration')
+  const events: string[] = []
 
-  const activationMessageIndex = consoleSpy
-    .get('startGroupCollapsed')
-    .findIndex((text) => {
-      return text.includes('[MSW] Mocking enabled')
+  const untilWorkerActivated = runtime.page
+    .evaluate(() => {
+      return new Promise((resolve) =>
+        navigator.serviceWorker.addEventListener('controllerchange', resolve),
+      )
+    })
+    .then(() => {
+      events.push('worker activated')
     })
 
-  const customMessageIndex = consoleSpy.get('log').findIndex((text) => {
-    return text.includes('Registration Promise resolved')
+  const untilStartResolved = runtime.page
+    .evaluate(() => {
+      return window.msw.startWorker()
+    })
+    .then(() => {
+      events.push('start resolved')
+    })
+
+  const untilActivationMessage = waitFor(() => {
+    expect(runtime.consoleSpy.get('startGroupCollapsed')).toContain(
+      '[MSW] Mocking enabled.',
+    )
+    events.push('enabled message')
   })
 
-  expect(activationMessageIndex).toBeGreaterThan(-1)
-  expect(customMessageIndex).toBeGreaterThan(-1)
-  expect(customMessageIndex).toBeGreaterThan(activationMessageIndex)
+  await Promise.all([
+    untilActivationMessage,
+    untilWorkerActivated,
+    untilStartResolved,
+  ])
+
+  expect(events[0]).toEqual('worker activated')
+  expect(events[1]).toEqual('start resolved')
+  expect(events[2]).toEqual('enabled message')
+  expect(events).toHaveLength(3)
 })

9815df990704dbc831856465ae3248b7220e31bf
Artem Zakharchenko <kettanaito@gmail.com>
2022-03-11 14:54:56 +0100

    chore: list all semantic-release plugins

--- release.config.js
+++ release.config.js
@@ -1,5 +1,11 @@
 /** @type {import('semantic-release').GlobalConfig} */
 module.exports = {
   branches: ['main'],
-  plugins: ['@semantic-release/git'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
 }

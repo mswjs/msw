9b1d171ce5bfe7de2d08af21fae8c45495e60ecf
Artem Zakharchenko <kettanaito@gmail.com>
2022-05-19 22:23:46 +0200

    fix: support typescript 4.4.x, 4.5.x, 4.6.x

--- src/utils/internal/tryCatch.ts
+++ src/utils/internal/tryCatch.ts
@@ -6,6 +6,6 @@ export function tryCatch<Fn extends (...args: any[]) => any>(
     const result = fn()
     return result
   } catch (error) {
-    onException?.(error)
+    onException?.(error as Error)
   }
 }

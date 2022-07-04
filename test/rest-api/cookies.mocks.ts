8805e4e79dc534f5c136d0f10450c266f4b4c938
Artem Zakharchenko <kettanaito@gmail.com>
2022-07-04 14:14:10 +0200

    fix: support multiple response cookies

--- test/rest-api/cookies.mocks.ts
+++ test/rest-api/cookies.mocks.ts
@@ -3,10 +3,17 @@ import { setupWorker, rest } from 'msw'
 const worker = setupWorker(
   rest.get('/user', (req, res, ctx) => {
     return res(
-      ctx.cookie('my-cookie', 'value'),
+      ctx.cookie('myCookie', 'value'),
       ctx.json({
         mocked: true,
       }),
+    )
+  }),
+  rest.get('/order', (req, res, ctx) => {
+    return res(
+      ctx.cookie('firstCookie', 'yes'),
+      ctx.cookie('secondCookie', 'no'),
+      ctx.json({ mocked: true }),
     )
   }),
 )
--- test/support/utils.ts
+++ test/support/utils.ts
@@ -80,3 +80,11 @@ export function promisifyChildProcess(
     })
   })
 }
+
+export function clearCookies(): void {
+  document.cookie.split(';').forEach((cookie) => {
+    document.cookie = cookie
+      .replace(/^ +/, '')
+      .replace(/=.*/, `=;expires=${new Date(0).toUTCString()};path=/`)
+  })
+}

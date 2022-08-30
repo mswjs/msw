50f6b9b1b08e0601acbb6ff55fcc93c3e0fd23f7
Artem Zakharchenko <kettanaito@gmail.com>
2022-08-30 13:15:47 +0200

    test(toReadonlyArray): ensure source is intact

--- src/utils/internal/toReadonlyArray.test.ts
+++ src/utils/internal/toReadonlyArray.test.ts
@@ -4,6 +4,14 @@ it('creates a copy of an array', () => {
   expect(toReadonlyArray([1, 2, 3])).toEqual([1, 2, 3])
 })
 
+it('does not affect the source array', () => {
+  const source = ['a', 'b', 'c']
+  toReadonlyArray(source)
+
+  expect(source.push('d')).toBe(4)
+  expect(source).toEqual(['a', 'b', 'c', 'd'])
+})
+
 it('forbids modifying the array copy', () => {
   const source = [1, 2, 3]
   const copy = toReadonlyArray(source)

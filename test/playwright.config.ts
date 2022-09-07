a421140f461166a9d07c9ff19ebbadcf4505705f
Artem Zakharchenko <kettanaito@gmail.com>
2022-09-08 00:55:49 +0200

    chore: remove unnecessary configs

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -4,9 +4,6 @@ const config: PlaywrightTestConfig = {
   workers: 1,
   globalSetup: require.resolve('./playwright.setup.ts'),
   forbidOnly: !!process.env.CI,
-  use: {
-    serviceWorkers: 'allow',
-  },
   projects: [
     {
       name: 'chromium',

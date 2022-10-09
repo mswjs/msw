aef1db1deb665d63c03fed649bdf7821efa0f55f
Artem Zakharchenko <kettanaito@gmail.com>
2022-10-09 18:15:48 +0200

    test(headers-multiple): separate headers by comma

--- test/rest-api/headers-multiple.mocks.ts
+++ test/rest-api/headers-multiple.mocks.ts
@@ -1,22 +1,24 @@
-import { setupWorker, rest } from 'msw'
+import { setupWorker, rest, HttpResponse } from 'msw'
 
 const worker = setupWorker(
-  rest.post('https://test.mswjs.io', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        'x-header': req.headers.get('x-header'),
-      }),
-    )
+  rest.post('https://test.mswjs.io', ({ request }) => {
+    return HttpResponse.json({
+      'x-header': request.headers.get('x-header'),
+    })
   }),
 
-  rest.get('https://test.mswjs.io', (req, res, ctx) => {
-    return res(
-      ctx.set({
-        Accept: ['application/json', 'image/png'],
-      }),
-      ctx.json({
+  rest.get('https://test.mswjs.io', () => {
+    return HttpResponse.json(
+      {
         mocked: true,
-      }),
+      },
+      {
+        headers: {
+          // List header values separated by comma
+          // to set multie-value header on the mocked response.
+          Accept: 'application/json, image/png',
+        },
+      },
     )
   }),
 )

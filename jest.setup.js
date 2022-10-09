f9840b7a0c4de4a95acf0c55cf2eff8ecc9f9ab9
Artem Zakharchenko <kettanaito@gmail.com>
2022-10-09 17:53:50 +0200

    chore: polyfill Request in jest tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch')
 
 if (typeof window !== 'undefined') {
+  globalThis.Request = fetch.Request
+
   // Provide "Headers" to be accessible in test cases
   // since they are not, by default.
   Object.defineProperty(window, 'Headers', {

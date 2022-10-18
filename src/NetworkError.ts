533f44c9f880a1d792b7a809b28eb68a09139ee6
Artem Zakharchenko <kettanaito@gmail.com>
2022-10-18 15:32:55 +0200

    chore(NetworkError): add jsdoc

--- src/NetworkError.ts
+++ src/NetworkError.ts
@@ -1,3 +1,14 @@
+/**
+ * An error indicating network issues while
+ * processing a request.
+ *
+ * @example
+ * import { rest, NetworkError } from 'msw'
+ *
+ * rest.get('/user', () => {
+ *   throw new NetworkError('Failed to fetch')
+ * })
+ */
 export class NetworkError extends Error {
   constructor(message: string) {
     super(message)

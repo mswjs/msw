8a0f925c9b40f437eb22b4d0eaade23bf3568dae
Artem Zakharchenko <kettanaito@gmail.com>
2022-11-12 11:03:51 +0100

    chore: use @swc/jest vs ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   bail: true,
   roots: ['<rootDir>/src', '<rootDir>/cli'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': '@swc/jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],

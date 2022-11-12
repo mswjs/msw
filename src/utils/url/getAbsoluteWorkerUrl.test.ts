1eba7fa223192cebc685b3acb956c8173aa7c930
Artem Zakharchenko <kettanaito@gmail.com>
2022-11-12 11:00:57 +0100

    fix: resolve absolute worker url against the current path

--- src/utils/url/getAbsoluteWorkerUrl.test.ts
+++ src/utils/url/getAbsoluteWorkerUrl.test.ts
@@ -3,12 +3,29 @@
  */
 import { getAbsoluteWorkerUrl } from './getAbsoluteWorkerUrl'
 
-describe('getAbsoluteWorkerUrl', () => {
-  describe('given a relative worker URL', () => {
-    it('should return an absolute URL against the current location', () => {
-      expect(getAbsoluteWorkerUrl('./mockServiceWorker.js')).toEqual(
-        'http://localhost/mockServiceWorker.js',
-      )
-    })
+const rawLocation = window.location
+
+afterAll(() => {
+  Object.defineProperty(window, 'location', {
+    value: rawLocation,
   })
+})
+
+it('returns absolute worker url relatively to the root', () => {
+  expect(getAbsoluteWorkerUrl('./worker.js')).toBe('http://localhost/worker.js')
+})
+
+it('returns an absolute worker url relatively to the current path', () => {
+  Object.defineProperty(window, 'location', {
+    value: {
+      href: 'http://localhost/path/to/page',
+    },
+  })
+
+  expect(getAbsoluteWorkerUrl('./worker.js')).toBe(
+    'http://localhost/path/to/worker.js',
+  )
+
+  // Leading slash must still resolve to the root.
+  expect(getAbsoluteWorkerUrl('/worker.js')).toBe('http://localhost/worker.js')
 })

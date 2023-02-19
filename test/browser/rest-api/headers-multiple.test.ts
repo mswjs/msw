410497b2f04c6e2f8d7db049105cdf8bab510638
Artem Zakharchenko <kettanaito@gmail.com>
2023-02-19 16:46:07 +0100

    chore: restore headers-multiple test

--- test/browser/rest-api/headers-multiple.test.ts
+++ test/browser/rest-api/headers-multiple.test.ts
@@ -3,31 +3,31 @@ import { test, expect } from '../playwright.extend'
 
 const EXAMPLE_PATH = require.resolve('./headers-multiple.mocks.ts')
 
-test.fixme(
-  'receives all headers from the request header with multiple values',
-  async ({ loadExample, fetch }) => {
-    await loadExample(EXAMPLE_PATH)
+test('receives all headers from the request header with multiple values', async ({
+  loadExample,
+  fetch,
+}) => {
+  await loadExample(EXAMPLE_PATH)
 
-    const headers = new Headers({ 'x-header': 'application/json' })
-    headers.append('x-header', 'application/hal+json')
+  const headers = new Headers({ 'x-header': 'application/json' })
+  headers.append('x-header', 'application/hal+json')
 
-    const res = await fetch('https://test.mswjs.io', {
-      method: 'POST',
-      headers: headers.all(),
-    })
-    const status = res.status()
-    const body = await res.json()
+  const res = await fetch('https://test.mswjs.io', {
+    method: 'POST',
+    headers: headers.all(),
+  })
+  const status = res.status()
+  const body = await res.json()
 
-    expect(status).toEqual(200)
-    expect(body).toEqual({
-      /**
-       * @fixme Multiple headers value becomes incompatible
-       * with the latest testing setup changes.
-       */
-      'x-header': 'application/json, application/hal+json',
-    })
-  },
-)
+  expect(status).toEqual(200)
+  expect(body).toEqual({
+    /**
+     * @fixme Multiple headers value becomes incompatible
+     * with the latest testing setup changes.
+     */
+    'x-header': 'application/json, application/hal+json',
+  })
+})
 
 test('supports setting a header with multiple values on the mocked response', async ({
   loadExample,

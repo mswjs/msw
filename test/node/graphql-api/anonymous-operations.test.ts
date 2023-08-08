ff049271d447bc8e4f35c6794ce7dfb9ec1e1fd7
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-08 19:14:58 +0200

    test(graphql): add test for anonymous operation warning

---
+++ test/node/graphql-api/anonymous-operations.test.ts
@@ -0,0 +1,108 @@
+/**
+ * @jest-environment node
+ */
+import fetch from 'node-fetch'
+import { HttpServer } from '@open-draft/test-server/http'
+import { HttpResponse, graphql } from 'msw'
+import { setupServer } from 'msw/node'
+
+const httpServer = new HttpServer((app) => {
+  app.post('/graphql', (req, res) => {
+    res.json({
+      data: {
+        user: { id: 'abc-123' },
+      },
+    })
+  })
+})
+
+const server = setupServer(graphql.query('GetUser', () => {}))
+
+beforeAll(async () => {
+  server.listen()
+  await httpServer.listen()
+  jest.spyOn(console, 'warn').mockImplementation(() => {})
+})
+
+afterEach(() => {
+  server.resetHandlers()
+  jest.resetAllMocks()
+})
+
+afterAll(async () => {
+  jest.restoreAllMocks()
+  server.close()
+  await httpServer.close()
+})
+
+test('warns on unhandled anonymous GraphQL operations', async () => {
+  const endpointUrl = httpServer.http.url('/graphql')
+  const response = await fetch(endpointUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          user {
+            id
+          }
+        }
+      `,
+    }),
+  })
+  const json = await response.json()
+
+  // Must receive the original server response.
+  expect(json).toEqual({
+    data: { user: { id: 'abc-123' } },
+  })
+
+  // Must print a warning about the anonymous GraphQL operation.
+  expect(console.warn).toHaveBeenCalledWith(`\
+[MSW] Failed to intercept a GraphQL request at "POST ${endpointUrl}": anonymous GraphQL operations are not supported.
+
+Consider naming this operation or using "graphql.operation()" request handler to intercept GraphQL requests regardless of their operation name/type. Read more: https://mswjs.io/docs/api/graphql/operation`)
+})
+
+test('does not print a warning when using anonymous operation with "graphql.operation()"', async () => {
+  server.use(
+    graphql.operation(async ({ query, variables }) => {
+      return HttpResponse.json({
+        data: {
+          pets: [{ name: 'Tom' }, { name: 'Jerry' }],
+        },
+      })
+    }),
+  )
+
+  const endpointUrl = httpServer.http.url('/graphql')
+  const response = await fetch(endpointUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          pets {
+            name
+          }
+        }
+      `,
+    }),
+  })
+  const json = await response.json()
+
+  // Must get the mocked response.
+  expect(json).toEqual({
+    data: {
+      pets: [{ name: 'Tom' }, { name: 'Jerry' }],
+    },
+  })
+
+  // Must print no warnings: operation is handled and doesn't
+  // have to be named since we're using "graphql.operation()".
+  expect(console.warn).not.toHaveBeenCalled()
+})

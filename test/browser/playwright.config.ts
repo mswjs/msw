9acafdeb395b7bec7ff9614f00a9a75a14650264
thepassle <pascalschilp@gmail.com>
2023-10-18 11:09:18 +0200

    chore: add more browsers to playwright config

--- test/browser/playwright.config.ts
+++ test/browser/playwright.config.ts
@@ -7,6 +7,14 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
   timeout: 10_000,
   retries: 1,

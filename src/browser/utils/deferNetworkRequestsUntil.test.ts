e4a49e5ed4e4c3dd8bd3218799bbd233604c200a
Artem Zakharchenko <kettanaito@gmail.com>
2023-11-16 15:45:02 +0100

    fix(deferNetworkUntil): restore module patches on registration rejections

--- src/browser/utils/deferNetworkRequestsUntil.test.ts
+++ src/browser/utils/deferNetworkRequestsUntil.test.ts
@@ -1,48 +1,167 @@
 /**
  * @vitest-environment jsdom
  */
+import { HttpServer } from '@open-draft/test-server/http'
 import { deferNetworkRequestsUntil } from './deferNetworkRequestsUntil'
 
-beforeAll(() => {
-  // Stubs native `fetch` function to remove any external
-  // asynchronicity from this test suite.
-  window.fetch = vi.fn().mockImplementation(() => {
-    return Promise.resolve({
-      json: () => ({
-        response: 'body',
-      }),
+const httpServer = new HttpServer((app) => {
+  app.get('/resource', (req, res) => {
+    res.send('hello world')
+  })
+})
+
+beforeAll(async () => {
+  await httpServer.listen()
+})
+
+afterAll(async () => {
+  await httpServer.close()
+})
+
+describe.sequential(deferNetworkRequestsUntil, () => {
+  test('defers fetch requests until the predicate promise resolves', async () => {
+    const events: Array<string> = []
+
+    // Emulate a Service Worker registration Promise.
+    const workerPromise = new Promise<null>((resolve) => {
+      setTimeout(resolve, 500)
     })
+
+    workerPromise.then(() => {
+      events.push('promise resolved')
+    })
+
+    // Calling this functions intercepts all requests that happen while
+    // the given promise is pending, and defers their execution
+    // until the promise is resolved.
+    deferNetworkRequestsUntil(workerPromise)
+
+    // Perform a request.
+    const responsePromise = fetch(httpServer.http.url('/resource')).then(
+      (response) => {
+        events.push('response received')
+        return response.text()
+      },
+    )
+
+    await Promise.all([workerPromise, responsePromise])
+    expect(events).toEqual(['promise resolved', 'response received'])
+
+    await expect(responsePromise).resolves.toBe('hello world')
+
+    // Assert the order of resolved events.
+    expect(window.fetch.name).toBe('fetch')
+    expect(window.XMLHttpRequest.prototype.send.name).toBe('send')
   })
-})
 
-afterAll(() => {
-  vi.restoreAllMocks()
-})
+  test('defers XHR requests until the predicate promise resolves', async () => {
+    const events: Array<string> = []
 
-test('defers any requests that happen while a given promise is pending', async () => {
-  const events: string[] = []
+    // Emulate a Service Worker registration Promise.
+    const workerPromise = new Promise<null>((resolve) => {
+      setTimeout(resolve, 500)
+    })
 
-  // Emulate a Service Worker registration Promise.
-  const workerPromise = new Promise<null>((resolve) => {
-    setTimeout(resolve, 1000)
+    workerPromise.then(() => {
+      events.push('promise resolved')
+    })
+
+    // Calling this functions intercepts all requests that happen while
+    // the given promise is pending, and defers their execution
+    // until the promise is resolved.
+    deferNetworkRequestsUntil(workerPromise)
+
+    // Perform a request.
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', httpServer.http.url('/resource'))
+    const responsePromise = new Promise<void>((resolve, reject) => {
+      xhr.onload = () => {
+        events.push('response received')
+        resolve()
+      }
+      xhr.onerror = reject
+      xhr.send()
+    })
+
+    await Promise.all([workerPromise, responsePromise])
+    expect(events).toEqual(['promise resolved', 'response received'])
+
+    expect(xhr.responseText).toBe('hello world')
+
+    // Assert the order of resolved events.
+    expect(window.fetch.name).toBe('fetch')
+    expect(window.XMLHttpRequest.prototype.send.name).toBe('send')
   })
 
-  workerPromise.then(() => {
-    events.push('promise resolved')
+  test('restores module patches if no requests were made and the predicate promise resolves', async () => {
+    const predicatePromise = new Promise((resolve) => {
+      setTimeout(resolve, 500)
+    })
+
+    deferNetworkRequestsUntil(predicatePromise)
+    await predicatePromise
+
+    // Assert the module names to be restored.
+    expect(window.fetch.name).toBe('fetch')
+    expect(window.XMLHttpRequest.prototype.send.name).toBe('send')
   })
 
-  // Calling this functions intercepts all requests that happen while
-  // the given promise is pending, and defers their execution
-  // until the promise is resolved.
-  deferNetworkRequestsUntil(workerPromise)
+  test('restores fetch if the request is pending and the predicate promise rejects', async () => {
+    const predicatePromise = new Promise((_, reject) => {
+      // Intentionally reject the perdicate promise.
+      reject(new Error('Failed to register a Service Worker'))
+    }).catch(() => void 0)
 
-  // Perform a request.
-  const requestPromise = fetch('/user').then(() => {
-    events.push('response received')
+    deferNetworkRequestsUntil(predicatePromise)
+
+    // Perform a request.
+    const responsePromise = fetch(httpServer.http.url('/resource')).then(
+      (response) => response.text(),
+    )
+
+    await expect(responsePromise).resolves.toBe('hello world')
+
+    // Assert the module names to be restored.
+    expect(window.fetch.name).toBe('fetch')
+    expect(window.XMLHttpRequest.prototype.send.name).toBe('send')
   })
 
-  await Promise.all([workerPromise, requestPromise])
+  test('restores XMLHttpRequest if the request is pending and the predicate promise rejects', async () => {
+    const predicatePromise = new Promise((_, reject) => {
+      // Intentionally reject the perdicate promise.
+      reject(new Error('Failed to register a Service Worker'))
+    }).catch(() => void 0)
 
-  // Assert the order of resolved events.
-  expect(events).toEqual(['promise resolved', 'response received'])
+    deferNetworkRequestsUntil(predicatePromise)
+
+    // Perform a request.
+    const xhr = new XMLHttpRequest()
+    const responsePromies = new Promise((resolve, reject) => {
+      xhr.onload = () => resolve(xhr.responseText)
+      xhr.onerror = reject
+      xhr.open('GET', httpServer.http.url('/resource'))
+      xhr.send()
+    })
+
+    await responsePromies
+    expect(xhr.responseText).toBe('hello world')
+
+    // Assert the module names to be restored.
+    expect(window.fetch.name).toBe('fetch')
+    expect(window.XMLHttpRequest.prototype.send.name).toBe('send')
+  })
+
+  test('restores module patches if no requests were made and the predicate promise rejects', async () => {
+    const predicatePromise = new Promise((_, reject) => {
+      // Intentionally reject the perdicate promise.
+      reject(new Error('Failed to register a Service Worker'))
+    }).catch(() => void 0)
+
+    deferNetworkRequestsUntil(predicatePromise)
+    await predicatePromise
+
+    // Assert the module names to be restored.
+    expect(window.fetch.name).toBe('fetch')
+    expect(window.XMLHttpRequest.prototype.send.name).toBe('send')
+  })
 })
--- src/browser/utils/deferNetworkRequestsUntil.ts
+++ src/browser/utils/deferNetworkRequestsUntil.ts
@@ -7,23 +7,42 @@ import { until } from '@open-draft/until'
  */
 export function deferNetworkRequestsUntil(predicatePromise: Promise<any>) {
   // Defer any `XMLHttpRequest` requests until the Service Worker is ready.
-  const originalXhrSend = window.XMLHttpRequest.prototype.send
-  window.XMLHttpRequest.prototype.send = function (
+  const originalXhrSend = globalThis.XMLHttpRequest.prototype.send
+
+  const restoreXMLHttpRequest = () => {
+    globalThis.XMLHttpRequest.prototype.send = originalXhrSend
+  }
+
+  globalThis.XMLHttpRequest.prototype.send = function deferredSend(
     ...args: Parameters<XMLHttpRequest['send']>
   ) {
     // Keep this function synchronous to comply with `XMLHttpRequest.prototype.send`,
     // because that method is always synchronous.
-    until(() => predicatePromise).then(() => {
-      window.XMLHttpRequest.prototype.send = originalXhrSend
-      this.send(...args)
-    })
+    until(() => predicatePromise)
+      /**
+       * @note That this will only be called if there has been a `send()` method
+       * invoked on the page.
+       */
+      .then(restoreXMLHttpRequest)
+      .finally(() => {
+        globalThis.XMLHttpRequest.prototype.send.apply(this, args)
+      })
   }
 
   // Defer any `fetch` requests until the Service Worker is ready.
-  const originalFetch = window.fetch
-  window.fetch = async (...args) => {
-    await until(() => predicatePromise)
-    window.fetch = originalFetch
-    return window.fetch(...args)
+  const originalFetch = globalThis.fetch
+
+  const restoreFetch = () => {
+    globalThis.fetch = originalFetch
   }
+
+  globalThis.fetch = async function deferredFetch(...args) {
+    await until(() => predicatePromise).then(restoreFetch)
+    return globalThis.fetch(...args)
+  }
+
+  predicatePromise.finally(() => {
+    restoreXMLHttpRequest()
+    restoreFetch()
+  })
 }

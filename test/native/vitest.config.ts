601099f5d1bcff147b74106c521264ba6db8ae5c
Artem Zakharchenko <kettanaito@gmail.com>
2023-11-24 15:21:05 +0100

    test: add "node:events" react native test

--- test/native/vitest.config.ts
+++ test/native/vitest.config.ts
@@ -1,28 +1,28 @@
 import * as path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
+const LIB_DIR = path.resolve(__dirname, '../../lib')
+
 export default defineConfig({
   test: {
-    /**
-     * @note Paths are resolved against CWD.
-     */
-    dir: './test/native',
+    root: './test/native',
+    include: ['**/*.native.test.ts'],
     globals: true,
+    setupFiles: ['./vitest.setup.ts'],
     alias: {
-      msw: path.resolve(__dirname, '../..'),
+      /**
+       * @note Force Vitest load ESM targets of MSW.
+       * If we run ESM in tests, we can use "vi.mock()" to
+       * emulate certain standard Node.js modules missing
+       * (like "node:events") in React Native.
+       *
+       * Vitest won't pick up the ESM targets because
+       * the root-level "package.json" is not "module".
+       */
+      'msw/node': path.resolve(LIB_DIR, 'node/index.mjs'),
+      'msw/native': path.resolve(LIB_DIR, 'native/index.mjs'),
+      'msw/browser': path.resolve(LIB_DIR, 'browser/index.mjs'),
+      msw: path.resolve(LIB_DIR, 'core/index.mjs'),
     },
-    environmentOptions: {
-      jsdom: {
-        url: 'http://localhost/',
-      },
-    },
-    /**
-     * @note Run Node.js integration tests in sequence.
-     * There's a test that involves building the library,
-     * which results in the "lib" directory being deleted.
-     * If any tests attempt to run during that window,
-     * they will fail, unable to resolve the "msw" import alias.
-     */
-    singleThread: true,
   },
 })

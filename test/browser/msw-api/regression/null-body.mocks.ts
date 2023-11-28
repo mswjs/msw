1d7ab5bddf95fdd2345ac5c0ab3a32f6703bc363
Matthew Costabile <mattcosta7@github.com>
2023-11-28 12:35:48 -0500

    test: inline status parse

--- test/browser/msw-api/regression/null-body.mocks.ts
+++ test/browser/msw-api/regression/null-body.mocks.ts
@@ -3,8 +3,7 @@ import { setupWorker } from 'msw/browser'
 
 const worker = setupWorker(
   http.get<{ code: string }>('/api/:code', ({ params }) => {
-    const status = parseInt(params.code)
-    return new HttpResponse(null, { status })
+    return new HttpResponse(null, { status: parseInt(params.code) })
   }),
 )
 

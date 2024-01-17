0d06a7dadc4cf9a2002479138e1982f9d172172e
Artem Zakharchenko <kettanaito@gmail.com>
2024-01-17 16:24:56 +0100

    test: add a mocked 206 response test

--- test/browser/rest-api/206-response.mocks.ts
+++ test/browser/rest-api/206-response.mocks.ts
@@ -1,5 +1,33 @@
+import { HttpResponse, http } from 'msw'
 import { setupWorker } from 'msw/browser'
 
-const worker = setupWorker()
+const data = 'hello world'
+const buffer = new TextEncoder().encode(data)
+const totalSize = buffer.byteLength
+
+const worker = setupWorker(
+  http.get('/mocked-range', ({ request }) => {
+    const range = request.headers.get('Range')
+
+    if (!range) {
+      throw HttpResponse.text('Missing Range', { status: 400 })
+    }
+
+    const ranges = range.replace(/bytes=/, '').split('-')
+    const start = +ranges[0]
+    const end = ranges[1] ? +ranges[1] : totalSize - 1
+    const content = buffer.slice(start, end)
+
+    return HttpResponse.arrayBuffer(content, {
+      status: 206,
+      headers: {
+        'Accept-Range': 'bytes',
+        'Content-Range': `bytes=${start}-${end}/${totalSize}`,
+        'Content-Length': content.byteLength.toString(),
+        'Content-Type': 'text/plain',
+      },
+    })
+  }),
+)
 
 worker.start()
--- test/browser/rest-api/206-response.test.ts
+++ test/browser/rest-api/206-response.test.ts
@@ -1,3 +1,6 @@
+/**
+ * @see https://github.com/mswjs/msw/issues/1972
+ */
 import { HttpServer } from '@open-draft/test-server/http'
 import { expect, test } from '../playwright.extend'
 
@@ -9,7 +12,7 @@ const server = new HttpServer((app) => {
   const totalSize = buffer.byteLength
 
   app.get('/range', (req, res) => {
-    const range = req.headers.range
+    const { range } = req.headers
 
     if (range) {
       const ranges = range.replace(/bytes=/, '').split('-')
@@ -46,6 +49,28 @@ test('forwards the 206 response to a bypassed "Range" request', async ({
   await loadExample(require.resolve('./206-response.mocks.ts'))
 
   const response = await fetch(server.http.url('/range'), {
+    headers: {
+      Range: 'bytes=2-8',
+    },
+  })
+
+  expect(response.status()).toBe(206)
+  expect(response.headers()).toMatchObject({
+    'accept-range': 'bytes',
+    'content-range': 'bytes=2-8/11',
+    'content-length': '6',
+    'content-type': 'text/plain',
+  })
+  expect(await response.text()).toBe('llo wo')
+})
+
+test('responds with a 206 response to a mocked "Range" request', async ({
+  loadExample,
+  fetch,
+}) => {
+  await loadExample(require.resolve('./206-response.mocks.ts'))
+
+  const response = await fetch('/mocked-range', {
     headers: {
       Range: 'bytes=2-8',
     },

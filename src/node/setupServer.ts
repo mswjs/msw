38f5d270491eb47dd23ceaee903c90a9ee639378
Artem Zakharchenko <kettanaito@gmail.com>
2024-02-09 12:11:45 +0100

    fix(setupServer): invalid return type

--- src/node/setupServer.ts
+++ src/node/setupServer.ts
@@ -2,8 +2,9 @@ import { ClientRequestInterceptor } from '@mswjs/interceptors/ClientRequest'
 import { XMLHttpRequestInterceptor } from '@mswjs/interceptors/XMLHttpRequest'
 import { FetchInterceptor } from '@mswjs/interceptors/fetch'
 import type { RequestHandler } from '~/core/handlers/RequestHandler'
-import { SetupServerApi } from './SetupServerApi'
 import type { WebSocketHandler } from '~/core/handlers/WebSocketHandler'
+import { SetupServerApi } from './SetupServerApi'
+import type { SetupServer } from './glossary'
 
 /**
  * Sets up a requests interception in Node.js with the given request handlers.
@@ -13,7 +14,7 @@ import type { WebSocketHandler } from '~/core/handlers/WebSocketHandler'
  */
 export const setupServer = (
   ...handlers: Array<RequestHandler | WebSocketHandler>
-) => {
+): SetupServer => {
   return new SetupServerApi(
     [ClientRequestInterceptor, XMLHttpRequestInterceptor, FetchInterceptor],
     ...handlers,

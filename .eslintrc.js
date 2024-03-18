23645d58a6808f1e1c9813ceb2b9ec1232a718a7
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-03-18 20:53:58 +0100

    remove redundant eslint config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,15 +19,6 @@ module.exports = {
     '@typescript-eslint/prefer-ts-expect-error': 2,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          Function: false,
-        },
-        extendDefaults: true,
-      },
-    ],
     '@typescript-eslint/ban-ts-comment': 0,
     '@typescript-eslint/no-namespace': [
       2,

f098c75b7b2780dbfb0c22d1a94c7a60e0343ea8
Artem Zakharchenko <kettanaito@gmail.com>
2024-03-18 18:07:33 +0100

    fix(setupWorker): remove left-over console.log()

--- src/core/typeUtils.ts
+++ src/core/typeUtils.ts
@@ -8,13 +8,13 @@ export type RequiredDeep<
 > = Type extends Fn
   ? Type
   : /**
-   * @note The "Fn" type satisfies the predicate below.
-   * It must always come first, before the Record check.
-   */
-  Type extends Record<string, any>
-  ? {
-      [Key in keyof Type]-?: NonNullable<Type[Key]> extends NonNullable<U>
-        ? NonNullable<Type[Key]>
-        : RequiredDeep<NonNullable<Type[Key]>, U>
-    }
-  : Type
+     * @note The "Fn" type satisfies the predicate below.
+     * It must always come first, before the Record check.
+     */
+    Type extends Record<string, any>
+    ? {
+        [Key in keyof Type]-?: NonNullable<Type[Key]> extends NonNullable<U>
+          ? NonNullable<Type[Key]>
+          : RequiredDeep<NonNullable<Type[Key]>, U>
+      }
+    : Type

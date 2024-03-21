fc44a2c83250f9fd4d6e5c9c7ef73fcb657127fe
Kai Spencer <51139521+KaiSpencer@users.noreply.github.com>
2024-03-21 11:39:43 +0000

    chore: use vitest for type testing (#2033)
    Co-authored-by: Artem Zakharchenko <kettanaito@gmail.com>

--- test/typings/http.test-d.ts
+++ test/typings/http.test-d.ts
@@ -1,132 +1,129 @@
+import { it, expectTypeOf } from 'vitest'
 import { http, HttpResponse, passthrough } from 'msw'
 
-/**
- * Request path parameters.
- */
-http.get<{ id: string }>('/user/:id', ({ params }) => {
-  params.id.toUpperCase()
+it('supports a single path parameter', () => {
+  http.get<{ id: string }>('/user/:id', ({ params }) => {
+    params.id.toUpperCase()
 
-  // @ts-expect-error Unknown path parameter
-  params.unknown
+    expectTypeOf(params).toEqualTypeOf<{ id: string }>()
+  })
 })
 
-http.get<{ a: string; b: string[] }>('/user/:a/:b/:b', ({ params }) => {
-  params.a.toUpperCase()
-  params.b.map((x) => x)
+it('supports multiple path parameters', () => {
+  http.get<{ a: string; b: string[] }>('/user/:a/:b/:b', ({ params }) => {
+    params.a.toUpperCase()
+    params.b.map((x) => x)
 
-  // @ts-expect-error Unknown path parameter
-  params.unknown
+    expectTypeOf(params).toEqualTypeOf<{ a: string; b: string[] }>()
+  })
 })
 
-// Supports path parameters declaration via type.
-type UserPathParams = { id: string }
-http.get<UserPathParams>('/user/:id', ({ params }) => {
-  params.id.toUpperCase()
+it('supports path parameters declared via type', () => {
+  type UserPathParams = { id: string }
+  http.get<UserPathParams>('/user/:id', ({ params }) => {
+    params.id.toUpperCase()
 
-  // @ts-expect-error Unknown path parameter
-  params.unknown
+    expectTypeOf(params).toEqualTypeOf<UserPathParams>()
+  })
 })
 
-// Supports path parameters declaration via interface.
-interface PostPathParameters {
-  id: string
-}
-http.get<PostPathParameters>('/user/:id', ({ params }) => {
-  params.id.toUpperCase()
-
-  // @ts-expect-error Unknown path parameter
-  params.unknown
-})
-
-http.get<never>('/user/:a/:b', ({ params }) => {
-  // @ts-expect-error Unknown path parameter
-  params.a.toUpperCase()
-  // @ts-expect-error Unknown path parameter
-  params.b.map((x) => x)
-})
-
-/**
- * Request body generic.
- */
-http.post<never, { id: string }>('/user', async ({ request }) => {
-  const data = await request.json()
-  data.id
-
-  // @ts-expect-error Unknown property
-  data.unknown
-
-  const text = await request.text()
-  text.toUpperCase()
-  // @ts-expect-error Text remains plain text.
-  text.id
-})
-
-http.get<never, null>('/user', async ({ request }) => {
-  const data = await request.json()
-  // @ts-expect-error Null is not an object
-  Object.keys(data)
-})
-
-/**
- * Response body generic.
- */
-http.get('/user', () => {
-  // Allows responding with a plain Response
-  // when no response body generic is set.
-  return new Response('hello')
-})
-
-http.get<never, never, { id: number }>('/user', () => {
-  return HttpResponse.json({ id: 1 })
-})
-
-// Supports explicit response data declared via type.
-type ResponseBodyType = { id: number }
-http.get<never, never, ResponseBodyType>('/user', () => {
-  const data: ResponseBodyType = { id: 1 }
-  return HttpResponse.json(data)
-})
-
-// Supports explicit response data declared via interface.
-interface ResponseBodyInterface {
-  id: number
-}
-http.get<never, never, ResponseBodyInterface>('/user', () => {
-  const data: ResponseBodyInterface = { id: 1 }
-  return HttpResponse.json(data)
-})
-
-http.get<never, never, { id: number }>(
-  '/user',
-  // @ts-expect-error String not assignable to number
-  () => HttpResponse.json({ id: 'invalid' }),
-)
-
-http.get<never, never, { id: number }>(
-  '/user',
-  // @ts-expect-error Missing property "id"
-  () => HttpResponse.json({}),
-)
-
-// Response resolver can return a response body of a
-// narrower type than defined in the generic.
-http.get<never, never, string | string[]>('/user', () =>
-  HttpResponse.json(['value']),
-)
-
-// Response resolver can return a more specific type
-// than provided in the response generic.
-http.get<never, never, { label: boolean }>('/user', () =>
-  HttpResponse.json({ label: true }),
-)
-
-// Passthrough responses.
-http.all('/', () => passthrough())
-http.get('/', () => passthrough())
-http.get<never, never, { id: number }>('/', ({ request }) => {
-  if (request.headers.has('cookie')) {
-    return passthrough()
+it('supports path parameters declared via interface', () => {
+  interface PostPathParameters {
+    id: string
   }
+  http.get<PostPathParameters>('/user/:id', ({ params }) => {
+    params.id.toUpperCase()
 
-  return HttpResponse.json({ id: 1 })
+    expectTypeOf(params).toEqualTypeOf<PostPathParameters>()
+  })
+})
+
+it('supports request body generic', () => {
+  http.post<never, { id: string }>('/user', async ({ request }) => {
+    const data = await request.json()
+
+    expectTypeOf(data).toEqualTypeOf<{ id: string }>()
+
+    const text = await request.text()
+    expectTypeOf(text).toEqualTypeOf<string>()
+    expectTypeOf(text).toEqualTypeOf<string>()
+  })
+})
+
+it('supports null as the response body generic', () => {
+  http.get<never, null>('/user', async ({ request }) => {
+    const data = await request.json()
+    expectTypeOf(data).toEqualTypeOf<null>()
+  })
+})
+
+it('returns plain Response withouth explicit response body generic', () => {
+  http.get('/user', () => {
+    return new Response('hello')
+  })
+})
+
+it('supports response body generic', () => {
+  http.get<never, never, { id: number }>('/user', () => {
+    return HttpResponse.json({ id: 1 })
+  })
+})
+
+it('supports response body generic declared via type', () => {
+  type ResponseBodyType = { id: number }
+  http.get<never, never, ResponseBodyType>('/user', () => {
+    const data: ResponseBodyType = { id: 1 }
+    return HttpResponse.json(data)
+  })
+})
+
+it('supports response body generic declared via interface', () => {
+  interface ResponseBodyInterface {
+    id: number
+  }
+  http.get<never, never, ResponseBodyInterface>('/user', () => {
+    const data: ResponseBodyInterface = { id: 1 }
+    return HttpResponse.json(data)
+  })
+})
+
+it('throws when returning a json response not matching the response body generic', () => {
+  http.get<never, never, { id: number }>(
+    '/user',
+    // @ts-expect-error String not assignable to number
+    () => HttpResponse.json({ id: 'invalid' }),
+  )
+})
+
+it('throws when returning an empty json response not matching the response body generic', () => {
+  http.get<never, never, { id: number }>(
+    '/user',
+    // @ts-expect-error Missing property "id"
+    () => HttpResponse.json({}),
+  )
+})
+
+it('accepts narrower type for response body', () => {
+  http.get<never, never, string | string[]>('/user', () =>
+    HttpResponse.json(['value']),
+  )
+})
+
+it('accepts more specific type for response body', () => {
+  http.get<never, never, { label: boolean }>('/user', () =>
+    HttpResponse.json({ label: true }),
+  )
+})
+
+it("accepts passthrough in HttpResponse's body", () => {
+  // Passthrough responses.
+  http.all('/', () => passthrough())
+  http.get('/', () => passthrough())
+  http.get<never, never, { id: number }>('/', ({ request }) => {
+    if (request.headers.has('cookie')) {
+      return passthrough()
+    }
+
+    return HttpResponse.json({ id: 1 })
+  })
 })

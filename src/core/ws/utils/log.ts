d6b886af2a7a9bae607148c1f8523971c1322663
Artem Zakharchenko <kettanaito@gmail.com>
2024-03-29 18:22:26 +0100

    fix: log incoming client events

--- src/core/ws/utils/log.ts
+++ src/core/ws/utils/log.ts
@@ -10,10 +10,13 @@ import { getMessageLength } from './getMessageLength'
 import { getPublicData } from './getPublicData'
 
 export function attachLogger(connection: WebSocketConnectionData): void {
-  const { client, server } = connection
+  const { client } = connection
 
   logConnectionOpen(client)
 
+  // Log the events sent from the WebSocket client.
+  // WebSocket client connection object is written from the
+  // server's perspective so these message events are outgoing.
   client.addEventListener('message', (event) => {
     logOutgoingMessage(event)
   })
@@ -22,7 +25,10 @@ export function attachLogger(connection: WebSocketConnectionData): void {
     logConnectionClose(event)
   })
 
-  server.addEventListener('message', (event) => {
+  // Log the events received by the WebSocket client.
+  // "client.socket" references the actual WebSocket instance
+  // so these message events are incoming messages.
+  client.socket.addEventListener('message', (event) => {
     logIncomingMessage(event)
   })
 }
@@ -87,7 +93,7 @@ function logConnectionClose(event: CloseEvent) {
   const publicUrl = toPublicUrl(target.url)
 
   console.groupCollapsed(
-    devUtils.formatMessage(`${getTimestamp()} %c▸%c ${publicUrl}`),
+    devUtils.formatMessage(`${getTimestamp()} %c■%c ${publicUrl}`),
     'color:blue',
     'color:inherit',
   )
--- test/browser/ws-api/ws.logging.browser.test.ts
+++ test/browser/ws-api/ws.logging.browser.test.ts
@@ -19,6 +19,10 @@ test.beforeAll(async () => {
   await server.listen()
 })
 
+test.afterEach(async () => {
+  server.resetState()
+})
+
 test.afterAll(async () => {
   await server.close()
 })
@@ -253,6 +257,72 @@ test('logs outgoing client event sending a long ArrayBuffer data', async ({
       expect.arrayContaining([
         expect.stringMatching(
           /^\[MSW\] \d{2}:\d{2}:\d{2}\.\d{3} %c↑%c ArrayBuffer\(this is an extremely lon…\) %c45%c color:green color:inherit color:gray;font-weight:normal color:inherit;font-weight:inherit$/,
+        ),
+      ]),
+    )
+  })
+})
+
+test('logs incoming client events', async ({
+  loadExample,
+  page,
+  spyOnConsole,
+}) => {
+  const consoleSpy = spyOnConsole()
+  await loadExample(require.resolve('./ws.runtime.js'), {
+    skipActivation: true,
+  })
+
+  server.addListener('connection', (ws) => {
+    ws.send('hello from server')
+
+    ws.addEventListener('message', (event) => {
+      if (event.data === 'how are you, server?') {
+        ws.send('thanks, not bad')
+      }
+    })
+  })
+
+  await page.evaluate(async (url) => {
+    const { setupWorker, ws } = window.msw
+    const api = ws.link(url)
+    const worker = setupWorker(
+      api.on('connection', ({ client, server }) => {
+        server.connect()
+        client.addEventListener('message', (event) => {
+          server.send(event.data)
+        })
+      }),
+    )
+    await worker.start()
+  }, server.url)
+
+  await page.evaluate((url) => {
+    const ws = new WebSocket(url)
+    ws.addEventListener('message', (event) => {
+      if (event.data === 'hello from server') {
+        ws.send('how are you, server?')
+      }
+    })
+  }, server.url)
+
+  // Initial message sent to every connected client.
+  await waitFor(() => {
+    expect(consoleSpy.get('raw')!.get('startGroupCollapsed')).toEqual(
+      expect.arrayContaining([
+        expect.stringMatching(
+          /^\[MSW\] \d{2}:\d{2}:\d{2}\.\d{3} %c↓%c hello from server %c17%c color:red color:inherit color:gray;font-weight:normal color:inherit;font-weight:inherit$/,
+        ),
+      ]),
+    )
+  })
+
+  // Message sent in response to a client message.
+  await waitFor(() => {
+    expect(consoleSpy.get('raw')!.get('startGroupCollapsed')).toEqual(
+      expect.arrayContaining([
+        expect.stringMatching(
+          /^\[MSW\] \d{2}:\d{2}:\d{2}\.\d{3} %c↓%c thanks, not bad %c15%c color:red color:inherit color:gray;font-weight:normal color:inherit;font-weight:inherit$/,
         ),
       ]),
     )

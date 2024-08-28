78360c881ac4ff4d6de32a7ea344cdd65c0c2ca1
Artem Zakharchenko <kettanaito@gmail.com>
2024-08-28 17:18:09 +0200

    chore(lint): set "no-console" rule to "error" (#2243)

--- src/core/utils/internal/devUtils.ts
+++ src/core/utils/internal/devUtils.ts
@@ -14,6 +14,7 @@ function formatMessage(message: string, ...positionals: any[]): string {
  * Prints a library-specific warning.
  */
 function warn(message: string, ...positionals: any[]): void {
+  // eslint-disable-next-line no-console
   console.warn(formatMessage(message, ...positionals))
 }
 
@@ -21,6 +22,7 @@ function warn(message: string, ...positionals: any[]): void {
  * Prints a library-specific error.
  */
 function error(message: string, ...positionals: any[]): void {
+  // eslint-disable-next-line no-console
   console.error(formatMessage(message, ...positionals))
 }
 

8a82c9c8866f63eba20604d297b0a896203cb3f8
Artem Zakharchenko <kettanaito@gmail.com>
2024-08-30 21:21:28 +0200

    fix: keep `graphql` import as require in cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,6 +7,7 @@ import {
 } from './config/plugins/esbuild/copyWorkerPlugin'
 import { resolveCoreImportsPlugin } from './config/plugins/esbuild/resolveCoreImportsPlugin'
 import { forceEsmExtensionsPlugin } from './config/plugins/esbuild/forceEsmExtensionsPlugin'
+import { graphqlImportPlugin } from './config/plugins/esbuild/graphQLImportPlugin'
 import packageJson from './package.json'
 
 // Externalize the in-house dependencies so that the user
@@ -33,7 +34,7 @@ const coreConfig: Options = {
   sourcemap: true,
   dts: true,
   tsconfig: path.resolve(__dirname, 'src/tsconfig.core.build.json'),
-  esbuildPlugins: [forceEsmExtensionsPlugin()],
+  esbuildPlugins: [graphqlImportPlugin(), forceEsmExtensionsPlugin()],
 }
 
 const nodeConfig: Options = {

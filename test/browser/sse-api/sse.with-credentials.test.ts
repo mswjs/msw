81cc17e251deca0471a7920dc5d4967b8c84c323
Artem Zakharchenko <kettanaito@gmail.com>
2024-10-01 18:01:07 +0200

    test: add "withCredentials" test

---
+++ test/browser/sse-api/sse.with-credentials.test.ts
@@ -0,0 +1,53 @@
+import { setupWorker, sse } from 'msw/browser'
+import { test, expect } from '../playwright.extend'
+
+declare namespace window {
+  export const msw: {
+    setupWorker: typeof setupWorker
+    sse: typeof sse
+  }
+}
+
+test('forwards document cookies on the request when "withCredentials" is set to true', async ({
+  loadExample,
+  page,
+  spyOnConsole,
+}) => {
+  const consoleSpy = spyOnConsole()
+  await loadExample(require.resolve('./sse.mocks.ts'), {
+    skipActivation: true,
+  })
+
+  await page.evaluate(() => {
+    document.cookie = 'foo=bar'
+  })
+
+  await page.evaluate(async () => {
+    const { setupWorker, sse } = window.msw
+
+    const worker = setupWorker(
+      sse('http://localhost/stream', ({ cookies }) => {
+        console.log(JSON.stringify({ cookies }))
+      }),
+    )
+    await worker.start()
+  })
+
+  await page.evaluate((url) => {
+    return new Promise<void>((resolve, reject) => {
+      const source = new EventSource('http://localhost/stream', {
+        withCredentials: true,
+      })
+      source.onopen = () => resolve()
+      source.onerror = () => reject(new Error('EventSource connection failed'))
+    })
+  })
+
+  expect(consoleSpy.get('log')).toContain(
+    JSON.stringify({
+      cookies: {
+        foo: 'bar',
+      },
+    }),
+  )
+})

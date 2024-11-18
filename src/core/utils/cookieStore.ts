bd41105486d923ca584fb123e17f3c72e2f69fb3
Sam Mousa <sam@mousa.nl>
2024-11-18 14:55:43 +0100

    fix: cleanup and further reduction of coupling

--- src/core/utils/cookieStore.ts
+++ src/core/utils/cookieStore.ts
@@ -1,5 +1,5 @@
-import { invariant } from 'outvariant'
 import { isNodeProcess } from 'is-node-process'
+import { invariant } from 'outvariant'
 import {
   Cookie,
   CookieJar,
@@ -7,60 +7,83 @@ import {
   MemoryCookieStoreIndex,
 } from 'tough-cookie'
 
-const storageKey = '__msw-cookie-store__'
-
-function loadFromLocalStorage(): MemoryCookieStoreIndex {
-  const json = localStorage.getItem('__msw-cookie-store__')
-
-  if (json == null) {
-    return {}
-  }
-
-  const rawCookies = JSON.parse(json) as Array<Record<string, any>>
-  const cookies: MemoryCookieStoreIndex = {}
-  for (const rawCookie of rawCookies) {
-    const cookie = Cookie.fromJSON(rawCookie)
-    if (cookie != null && cookie.domain != null && cookie.path != null) {
-      cookies[cookie.domain][cookie.path][cookie.key] = cookie
-    }
-  }
-  return cookies
+type SimpleCookie = {
+  asString: string
+  key: string
+  value: string
 }
 
-function storeInLocalStorage(idx: MemoryCookieStoreIndex): void {
-  const data = []
-  for (const domain in idx) {
-    for (const path in idx[domain]) {
-      for (const key in idx[domain][path]) {
-        data.push(idx[domain][path][key].toJSON())
+class SimpleStore {
+  private readonly storageKey = '__msw-cookie-store__'
+  private readonly jar: CookieJar
+  private readonly memoryStore: MemoryCookieStore
+
+  constructor() {
+    const memoryStore = new MemoryCookieStore()
+    if (!isNodeProcess()) {
+      invariant(
+        typeof localStorage !== 'undefined',
+        'Failed to create a WebStorageCookieStore: `localStorage` is not available in this environment. This is likely an issue with MSW. Please report it on GitHub: https://github.com/mswjs/msw/issues',
+      )
+    }
+    memoryStore.idx = this.loadFromLocalStorage()
+
+    this.jar = new CookieJar(memoryStore)
+    this.memoryStore = memoryStore
+  }
+
+  private loadFromLocalStorage(): MemoryCookieStoreIndex {
+    if (typeof localStorage === 'undefined') {
+      return {}
+    }
+
+    const json = localStorage.getItem(this.storageKey)
+
+    if (json == null) {
+      return {}
+    }
+
+    const rawCookies = JSON.parse(json) as Array<Record<string, any>>
+    const cookies: MemoryCookieStoreIndex = {}
+    for (const rawCookie of rawCookies) {
+      const cookie = Cookie.fromJSON(rawCookie)
+      if (cookie != null && cookie.domain != null && cookie.path != null) {
+        cookies[cookie.domain][cookie.path][cookie.key] = cookie
       }
     }
+    return cookies
   }
 
-  localStorage.setItem(storageKey, JSON.stringify(data))
-}
-
-const memoryStore = new MemoryCookieStore()
-// Instead of implementing our own cookie store we simply proxy the memory store.
-if (!isNodeProcess()) {
-  invariant(
-    typeof localStorage !== 'undefined',
-    'Failed to create a WebStorageCookieStore: `localStorage` is not available in this environment. This is likely an issue with MSW. Please report it on GitHub: https://github.com/mswjs/msw/issues',
-  )
-  memoryStore.idx = loadFromLocalStorage()
-}
-
-const cookieJar = new CookieJar(memoryStore)
-
-export const cookieStore = {
-  setCookie(cookie: string | Cookie, url: string | undefined): void {
-    if (typeof url !== 'undefined') {
-      cookieJar.setCookieSync(cookie, url)
-      storeInLocalStorage(memoryStore.idx)
+  private storeInLocalStorage(): void {
+    if (typeof localStorage === 'undefined') {
+      return
     }
-  },
 
-  getCookiesSync(url: string): Cookie[] {
-    return cookieJar.getCookiesSync(url)
-  },
+    const data = []
+    const idx = this.memoryStore.idx
+    for (const domain in idx) {
+      for (const path in idx[domain]) {
+        for (const key in idx[domain][path]) {
+          data.push(idx[domain][path][key].toJSON())
+        }
+      }
+    }
+    localStorage.setItem(this.storageKey, JSON.stringify(data))
+  }
+
+  async setCookie(cookie: string, url: string): Promise<void> {
+    await this.jar.setCookie(cookie, url)
+    this.storeInLocalStorage()
+    return
+  }
+
+  getCookies(url: string): SimpleCookie[] {
+    return this.jar.getCookiesSync(url).map((c: Cookie) => ({
+      key: c.key,
+      value: c.value,
+      asString: c.toString(),
+    }))
+  }
 }
+
+export const cookieStore = new SimpleStore()

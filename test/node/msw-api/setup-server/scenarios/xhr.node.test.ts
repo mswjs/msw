fe601194abc94c058b4333060c657810b4b1a590
Lars-Jørgen Kristiansen <1528255+LarsJK@users.noreply.github.com>
2024-11-29 13:24:33 +0100

    test: add XHR file upload test

--- test/node/msw-api/setup-server/scenarios/xhr.node.test.ts
+++ test/node/msw-api/setup-server/scenarios/xhr.node.test.ts
@@ -71,3 +71,49 @@ describe('given I perform an XMLHttpRequest', () => {
     })
   })
 })
+
+describe('given I perform an XMLHttpRequest with a file', () => {
+  let statusCode: number
+  let headers: Headers
+  let body: string
+
+  beforeAll(async () => {
+    const file = new File(['{}'], 'test.json', { type: 'application/json' })
+
+    const req = new XMLHttpRequest()
+    req.open('GET', 'http://localhost:3001/resource')
+
+    req.setRequestHeader('Content-Type', 'multipart/form-data')
+    const requestFinishPromise = new Promise<void>((resolve, reject) => {
+      req.onload = function () {
+        statusCode = this.status
+        body = JSON.parse(this.response)
+        headers = stringToHeaders(this.getAllResponseHeaders())
+        resolve()
+      }
+      req.onerror = reject.bind(req)
+    })
+
+    const fileData = new FormData()
+    fileData.append('file', file)
+    req.send(fileData)
+
+    await requestFinishPromise
+  })
+
+  test('returns mocked status code', () => {
+    expect(statusCode).toEqual(401)
+  })
+
+  test('returns mocked headers', () => {
+    expect(headers.get('content-type')).toEqual('application/json')
+    expect(headers.get('x-header')).toEqual('yes')
+  })
+
+  test('returns mocked body', () => {
+    expect(body).toEqual({
+      firstName: 'John',
+      age: 32,
+    })
+  })
+})

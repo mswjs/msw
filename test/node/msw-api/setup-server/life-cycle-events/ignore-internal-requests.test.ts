ac6da8bb0e1cbdfa447a956a076350c1981a62fb
Artem Zakharchenko <kettanaito@gmail.com>
2025-01-08 13:51:50 +0100

    test(setupServer): add test for not emitting life cycle events on internal requests

--- test/node/msw-api/setup-server/life-cycle-events/ignore-internal-requests.test.ts
+++ test/node/msw-api/setup-server/life-cycle-events/ignore-internal-requests.test.ts
@@ -1,17 +1,13 @@
 // @vitest-environment node
 import { setupServer } from 'msw/node'
-import {
-  MSW_REMOTE_SERVER_URL,
-  MSW_REMOTE_BOUNDARY_ID,
-} from '../../../../../src/node/remoteContext'
 import { spyOnLifeCycleEvents } from '../../../../support/utils'
 
 const server = setupServer()
 
 beforeAll(() => {
   // Mock the environment variables required for the remote interception to work.
-  vi.stubEnv(MSW_REMOTE_SERVER_URL, 'http://localhost/noop')
-  vi.stubEnv(MSW_REMOTE_BOUNDARY_ID, 'abc-123')
+  vi.stubEnv('MSW_REMOTE_SERVER_URL', 'http://localhost/noop')
+  vi.stubEnv('MSW_REMOTE_BOUNDARY_ID', 'abc-123')
 
   server.listen({
     // Enable remote interception to trigger internal requests.

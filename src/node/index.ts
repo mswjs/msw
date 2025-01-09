b22d00c00ecb904f740a61a14c2a739725cb522b
Artem Zakharchenko <kettanaito@gmail.com>
2025-01-09 19:05:26 +0100

    fix(node): export `ListenOptions`

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,4 +1,4 @@
-export type { SetupServer } from './glossary'
+export type { SetupServer, ListenOptions } from './glossary'
 export { SetupServerApi } from './SetupServerApi'
 export { setupServer } from './setupServer'
 export {

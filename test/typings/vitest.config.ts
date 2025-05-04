032bf081d7a22555d558d779e13ead17a5ec7fd9
Artem Zakharchenko <kettanaito@gmail.com>
2025-05-04 22:41:17 +0200

    chore: use inline msw aliases

--- test/typings/vitest.config.ts
+++ test/typings/vitest.config.ts
@@ -3,7 +3,8 @@ import * as url from 'node:url'
 import { defineConfig } from 'vitest/config'
 import { invariant } from 'outvariant'
 import tsPackageJson from 'typescript/package.json' assert { type: 'json' }
-import { mswExports } from '../support/alias'
+
+const LIB_DIR = new URL('../../lib/', import.meta.url)
 
 export default defineConfig({
   test: {
@@ -40,7 +41,10 @@ export default defineConfig({
       })(),
     },
     alias: {
-      ...mswExports,
+      'msw/node': url.fileURLToPath(new URL('node/index.js', LIB_DIR)),
+      'msw/native': url.fileURLToPath(new URL('native/index.js', LIB_DIR)),
+      'msw/browser': url.fileURLToPath(new URL('browser/index.js', LIB_DIR)),
+      msw: url.fileURLToPath(new URL('core/index.js', LIB_DIR)),
     },
   },
 })

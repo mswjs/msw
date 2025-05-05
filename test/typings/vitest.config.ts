752514a68e807967e3ef196447468f76a35cf6e0
Artem Zakharchenko <kettanaito@gmail.com>
2025-05-05 19:01:00 +0200

    chore: refactor test:ts setup

--- test/typings/vitest.config.ts
+++ test/typings/vitest.config.ts
@@ -1,17 +1,26 @@
 import fs from 'node:fs'
 import url from 'node:url'
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import { invariant } from 'outvariant'
 import tsPackageJson from 'typescript/package.json' assert { type: 'json' }
 import { mswExports } from '../support/alias'
 
+const TEST_ROOT = url.fileURLToPath(new URL('./', import.meta.url))
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      ...mswExports,
+    },
+  },
   test: {
-    root: './test/typings',
-    include: ['**/*.test-d.ts'],
+    root: TEST_ROOT,
     globals: true,
     typecheck: {
+      enabled: true,
       checker: 'tsc',
+      include: ['**/*.test-d.ts'],
       tsconfig: (() => {
         const tsInstalledVersion = tsPackageJson.version
         invariant(
@@ -26,21 +35,19 @@ export default defineConfig({
 
         const tsConfigPaths = [
           url.fileURLToPath(
-            new URL(`tsconfig.${tsVersionMajorMinor}.json`, import.meta.url),
+            new URL(`./tsconfig.${tsVersionMajorMinor}.json`, import.meta.url),
           ),
-          url.fileURLToPath(new URL('tsconfig.json', import.meta.url)),
+          url.fileURLToPath(new URL('./tsconfig.json', import.meta.url)),
         ]
         const tsConfigPath = tsConfigPaths.find((path) =>
           fs.existsSync(path),
         ) as string
+        const relativeTsConfigPath = path.relative(TEST_ROOT, tsConfigPath)
 
-        console.log('Using tsconfig at: %s', tsConfigPath)
+        console.log('Using tsconfig at: %s', relativeTsConfigPath)
 
-        return tsConfigPath
+        return relativeTsConfigPath
       })(),
-    },
-    alias: {
-      ...mswExports,
     },
   },
 })

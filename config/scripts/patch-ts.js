d18827d9af34b157f02e4696d9eddb15d8549dbe
Artem Zakharchenko <kettanaito@gmail.com>
2025-05-09 10:33:53 +0200

    chore: validate for no .mjs refs in .d.ts files

--- config/scripts/patch-ts.js
+++ config/scripts/patch-ts.js
@@ -1,5 +1,4 @@
 import fs from 'node:fs'
-import url from 'node:url'
 import { exec } from 'node:child_process'
 import { promisify } from 'node:util'
 import { invariant } from 'outvariant'
@@ -8,7 +7,7 @@ import { hasCoreImports, replaceCoreImports } from '../replaceCoreImports.js'
 
 const execAsync = promisify(exec)
 
-const BUILD_DIR = url.fileURLToPath(new URL('../../lib', import.meta.url))
+const BUILD_DIR = new URL('../../lib/', import.meta.url)
 
 async function patchTypeDefs() {
   const typeDefsPaths = glob.sync('**/*.d.{ts,mts}', {
@@ -59,7 +58,7 @@ async function patchTypeDefs() {
 
   // Next, validate that we left no "~/core" imports unresolved.
   const result = await execAsync(
-    `grep "~/core" ./**/*.{ts,mts} -R -l || exit 0`,
+    `grep "~/core" ./**/*.d.{ts,mts} -R -l || exit 0`,
     {
       cwd: BUILD_DIR,
       shell: '/bin/bash',
@@ -80,7 +79,7 @@ async function patchTypeDefs() {
     console.error(
       `Found .d.ts modules containing unresolved "~/core" import after the patching:
 
-  ${modulesWithUnresolvedImports.map((path) => `  - ${path}`).join('\n')}
+${modulesWithUnresolvedImports.map((path) => `  - ${new URL(path, BUILD_DIR).pathname}`).join('\n')}
         `,
     )
 
@@ -100,6 +99,36 @@ async function patchTypeDefs() {
     console.error(
       'Failed to compile the .d.ts modules with tsc. See the original error below.',
       tscCompilation.stderr,
+    )
+
+    return process.exit(1)
+  }
+
+  // Ensure that CJS .d.ts file never reference .mjs files.
+  const mjsInCjsResult = await execAsync(
+    `grep ".mjs" ./**/*.d.ts -R -l || exit 0`,
+    {
+      cwd: BUILD_DIR,
+      shell: '/bin/bash',
+    },
+  )
+
+  invariant(
+    mjsInCjsResult.stderr === '',
+    'Failed to validate the .d.ts modules not referencing ".mjs" files. See the original error below.',
+    mjsInCjsResult.stderr,
+  )
+
+  if (mjsInCjsResult.stdout !== '') {
+    const modulesWithUnresolvedImports = mjsInCjsResult.stdout
+      .split('\n')
+      .filter(Boolean)
+
+    console.error(
+      `Found .d.ts modules referencing ".mjs" files after patching:
+
+${modulesWithUnresolvedImports.map((path) => `  - ${new URL(path, BUILD_DIR).pathname}`).join('\n')}
+        `,
     )
 
     return process.exit(1)

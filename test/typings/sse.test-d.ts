3b125435ae4e01cfa5d0a4593a08f7a36a80dbb6
Artem Zakharchenko <kettanaito@gmail.com>
2025-06-04 09:44:06 +0200

    test: move optional data type to its own test case

--- test/typings/sse.test-d.ts
+++ test/typings/sse.test-d.ts
@@ -55,79 +55,77 @@ it('supports custom event map type', () => {
 })
 
 it('supports event map type argument for unnamed events', () => {
-  sse<{ message: number; custom: string; other: boolean; maybe?: string }>('/stream', ({ client }) => {
-    /**
-     * @note TS 5.0 reports errors at different locations
-     * to satisfy a wide range of TS versions in type tests formatting gets disabled here
-     * so that @ts-expect-error can assert the reported errors regardless of the location
-     * see: https://github.com/mswjs/msw/pull/2521#issuecomment-2931733565
-     */
-    // prettier-ignore-start
-    client.send({
-      data: 123,
-    })
-    // When no explicit "event" key is provided,
-    // threat it as if event was set to "message".
-    // @ts-expect-error Unexpected data type for "message" event.
-    client.send({ data: 'goodbye' })
+  sse<{ message: number; custom: string; other: boolean }>(
+    '/stream',
+    ({ client }) => {
+      /**
+       * @note TS 5.0 reports errors at different locations
+       * to satisfy a wide range of TS versions in type tests formatting gets disabled here
+       * so that @ts-expect-error can assert the reported errors regardless of the location
+       * see: https://github.com/mswjs/msw/pull/2521#issuecomment-2931733565
+       */
+      // prettier-ignore-start
+      client.send({
+        data: 123,
+      })
+      // When no explicit "event" key is provided,
+      // threat it as if event was set to "message".
+      // @ts-expect-error Unexpected data type for "message" event.
+      client.send({ data: 'goodbye' })
 
-    client.send({
-      event: 'message',
-      data: 123,
-    })
-    // @ts-expect-error Unexpected data type for "message" event.
-    client.send({ event: 'message', data: 'invalid' })
-    // @ts-expect-error Unexpected data type for "message" event.
-    client.send({ event: 'message', data: 'goodbye' })
+      client.send({
+        event: 'message',
+        data: 123,
+      })
+      // @ts-expect-error Unexpected data type for "message" event.
+      client.send({ event: 'message', data: 'invalid' })
+      // @ts-expect-error Unexpected data type for "message" event.
+      client.send({ event: 'message', data: 'goodbye' })
 
-    client.send({
-      event: 'custom',
-      data: 'goodbye',
-    })
-    // @ts-expect-error Unexpected data type for "custom" event
-    client.send({ event: 'custom', data: 123 })
+      client.send({
+        event: 'custom',
+        data: 'goodbye',
+      })
+      // @ts-expect-error Unexpected data type for "custom" event
+      client.send({ event: 'custom', data: 123 })
 
-    // Sending unknown events must be forbidden
-    // if the EventMap type argument was provided.
-    // @ts-expect-error Unknown event type "unknown".
-    client.send({ event: 'invalid', data: 123 })
+      // Sending unknown events must be forbidden
+      // if the EventMap type argument was provided.
+      // @ts-expect-error Unknown event type "unknown".
+      client.send({ event: 'invalid', data: 123 })
 
-    // boolean is only allowed for "other" event
-    // @ts-expect-error
-    client.send({ event: 'custom', data: true })
-
-    // boolean is only allowed for "other" event
-    // @ts-expect-error
-    client.send({ event: 'custom' as 'custom' | 'other', data: true })
-
-    // should error when required data is missing
-    // @ts-expect-error
-    client.send({ event: 'other' })
-
-    // should allow missing data when it's optional
-    client.send({
-      event: 'maybe',
-    })
-
-    // should allow undefined data when it's optional
-    client.send({
-      event: 'maybe',
-      data: undefined,
-    })
-
-    // should allow valid data when it's optional
-    client.send({
-      event: 'maybe',
-      data: 'awesome',
-    })
-
-    client.send({
-      event: 'maybe',
-      // should reject invalid data even when it's optional
+      // boolean is only allowed for "other" event
       // @ts-expect-error
+      client.send({ event: 'custom', data: true })
+
+      // boolean is only allowed for "other" event
+      // @ts-expect-error
+      client.send({ event: 'custom' as 'custom' | 'other', data: true })
+
+      // should error when required data is missing
+      // @ts-expect-error
+      client.send({ event: 'other' })
+
+      // prettier-ignore-end
+    },
+  )
+})
+
+it('supports optional event data', () => {
+  sse<{ maybe?: string }>('/stream', ({ client }) => {
+    // No data is fine because data is optional.
+    client.send({ event: 'maybe' })
+
+    // Allows explicit undefined as optional data.
+    client.send({ event: 'maybe', data: undefined })
+
+    // Data is still validated even if optional.
+    client.send({ event: 'maybe', data: 'hello' })
+    client.send({
+      event: 'maybe',
+      // @ts-expect-error Invalid data type.
       data: 123,
     })
-    // prettier-ignore-end
   })
 })
 
@@ -140,16 +138,13 @@ it('supports sending custom retry duration', () => {
      * see: https://github.com/mswjs/msw/pull/2521#issuecomment-2931733565
      */
     // prettier-ignore-start
-    client.send({
-      // "retry" must be the only property on the message.
-      retry: 1000,
-    })
+    client.send({ retry: 1000 })
 
-    // @ts-expect-error No properties are allowed.
+    // @ts-expect-error Cannot use message properties with "retry".
     client.send({ retry: 1000, id: '1' })
-    // @ts-expect-error No properties are allowed.
+    // @ts-expect-error Cannot use message properties with "retry".
     client.send({ retry: 1000, data: 'hello' })
-    // @ts-expect-error No properties are allowed.
+    // @ts-expect-error Cannot use message properties with "retry".
     client.send({ retry: 1000, event: 'custom', data: 'goodbye' })
     // prettier-ignore-end
   })

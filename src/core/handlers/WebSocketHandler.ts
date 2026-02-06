1e3ab422bfb14119ca6d93358bec08453dfcb67b
Artem Zakharchenko <kettanaito@gmail.com>
2026-02-06 15:40:38 +0100

    fix(WebSocketHandler): omit trailing slash from the resolved url

--- src/core/handlers/WebSocketHandler.ts
+++ src/core/handlers/WebSocketHandler.ts
@@ -44,20 +44,10 @@ export class WebSocketHandler {
   public id: string
   public callFrame?: string
 
-  protected readonly url: Path
   protected [kEmitter]: Emitter<WebSocketHandlerEventMap>
 
-  constructor(url: Path) {
+  constructor(protected readonly url: Path) {
     this.id = createRequestId()
-
-    // Resolve the WebSocket handler path:
-    // - Plain string URLs resolved as per the specification (via Interceptors).
-    // - String URLs starting with a wildcard are preserved (prepending a scheme there will break them).
-    // - RegExp paths are preserved.
-    this.url =
-      url instanceof RegExp || url.startsWith('*')
-        ? url
-        : resolveWebSocketUrl(url)
 
     this[kEmitter] = new Emitter()
     this.callFrame = getCallFrame(new Error())
@@ -70,6 +60,15 @@ export class WebSocketHandler {
   }): WebSocketHandlerParsedResult {
     const clientUrl = new URL(args.url)
 
+    // Resolve the WebSocket handler path:
+    // - Plain string URLs resolved as per the specification (via Interceptors).
+    // - String URLs starting with a wildcard are preserved (prepending a scheme there will break them).
+    // - RegExp paths are preserved.
+    const resolvedHandlerUrl =
+      this.url instanceof RegExp || this.url.startsWith('*')
+        ? this.url
+        : this.#resolveWebSocketUrl(this.url, args.resolutionContext?.baseUrl)
+
     /**
      * @note Remove the Socket.IO path prefix from the WebSocket
      * client URL. This is an exception to keep the users from
@@ -79,7 +78,7 @@ export class WebSocketHandler {
 
     const match = matchRequestUrl(
       clientUrl,
-      this.url,
+      resolvedHandlerUrl,
       args.resolutionContext?.baseUrl,
     )
 
@@ -147,6 +146,26 @@ export class WebSocketHandler {
     // Emit the connection event on the handler.
     // This is what the developer adds listeners for.
     return this[kEmitter].emit('connection', connection)
+  }
+
+  #resolveWebSocketUrl(url: string, baseUrl?: string): string {
+    const resolvedUrl = resolveWebSocketUrl(
+      baseUrl
+        ? /**
+           * @note Resolve against the base URL preemtively because `resolveWebSocketUrl` only
+           * resolves against `location.href`, which is missing in Node.js. Base URL allows
+           * the handler to accept a relative URL in Node.js.
+           */
+          new URL(url, baseUrl)
+        : url,
+    )
+
+    /**
+     * @note Omit the trailing slash.
+     * While the browser always produces a trailing slash at the end of a WebSocket URL,
+     * having it in as the handler's predicate would mean it is *required* in the actual URL.
+     */
+    return resolvedUrl.replace(/\/$/, '')
   }
 }
 

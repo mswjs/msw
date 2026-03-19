85e4708949d52cc8e21b2bdbb7839a204cf7a7b4
Artem Zakharchenko <kettanaito@gmail.com>
2026-03-19 18:28:00 +0100

    test(request-handler-type): improve the type test

--- test/typings/regressions/request-handler-type.test-d.ts
+++ test/typings/regressions/request-handler-type.test-d.ts
@@ -1,28 +1,76 @@
 /**
  * @see https://github.com/mswjs/msw/discussions/498
  */
-import { http, HttpResponse, type RequestHandler } from 'msw'
-import { setupWorker } from 'msw/browser'
+import {
+  http,
+  graphql,
+  HttpHandler,
+  ws,
+  RequestHandler,
+  type AnyHandler,
+  type WebSocketHandler,
+  type GraphQLHandler,
+} from 'msw'
 import { setupServer } from 'msw/node'
+import { setupWorker } from 'msw/browser'
 
-const handlers: Array<RequestHandler> = [
-  http.get('/resource', () => {
-    return HttpResponse.json({ data: 'abc-123' })
-  }),
-]
-
-it('does not conflict when passing request handlers to `setupWorker`', () => {
-  setupWorker(...handlers)
+it('handler types extend the AnyHandler type', () => {
+  expectTypeOf<RequestHandler>().toExtend<AnyHandler>()
+  expectTypeOf<HttpHandler>().toExtend<AnyHandler>()
+  expectTypeOf<GraphQLHandler>().toExtend<AnyHandler>()
+  expectTypeOf<WebSocketHandler>().toExtend<AnyHandler>()
 })
 
-it('does not conflict when passing request handlers to `setupServer`', () => {
-  setupServer(...handlers)
+it('http handlers extend the RequestHandler type', () => {
+  const handlers = [http.get('/', () => {})]
+
+  expectTypeOf<HttpHandler>().toExtend<RequestHandler>()
+  expectTypeOf(handlers).toExtend<Array<RequestHandler>>()
+  expectTypeOf(handlers).toEqualTypeOf<Array<HttpHandler>>()
 })
 
-it('does not conflict when inferring request handlers for `setupWorker`', () => {
-  const resourceHandler = http.get('/resource', () => {
-    return HttpResponse.json({ data: 'abc-123' })
-  })
+it('graphql handlers extend the RequestHandler type', () => {
+  const handlers = [graphql.query('GetUser', () => {})]
 
-  setupWorker(...[resourceHandler])
+  expectTypeOf<GraphQLHandler>().toExtend<RequestHandler>()
+  expectTypeOf(handlers).toExtend<Array<RequestHandler>>()
+  expectTypeOf(handlers).toEqualTypeOf<Array<GraphQLHandler>>()
+})
+
+it('a list of http and graphql handlers extend the RequestHandler type', () => {
+  const handlers = [http.get('/', () => {}), graphql.query('GetUser', () => {})]
+
+  expectTypeOf(handlers).toExtend<Array<RequestHandler>>()
+  expectTypeOf(handlers).toEqualTypeOf<Array<HttpHandler | GraphQLHandler>>()
+})
+
+it('websocket handler extends the WebSocketHandler type', () => {
+  expectTypeOf([ws.link('/').addEventListener('connection', () => {})])
+    .toEqualTypeOf<Array<WebSocketHandler>>
+})
+
+it('accepts different handler types in setupWorker', () => {
+  expectTypeOf(setupWorker).parameters.toEqualTypeOf<Array<AnyHandler>>()
+  expectTypeOf(setupWorker).parameters.toExtend<
+    Array<RequestHandler | WebSocketHandler>
+  >()
+})
+
+it('accepts narrower handler types in setupWorker', () => {
+  setupWorker(...([] as Array<HttpHandler>))
+  setupWorker(...([] as Array<GraphQLHandler>))
+  setupWorker(...([] as Array<WebSocketHandler>))
+})
+
+it('accepts different handler types in setupServer', () => {
+  expectTypeOf(setupServer).parameters.toEqualTypeOf<Array<AnyHandler>>()
+  expectTypeOf(setupServer).parameters.toExtend<
+    Array<RequestHandler | WebSocketHandler>
+  >()
+})
+
+it('accepts narrower handler types in setupServer', () => {
+  setupServer(...([] as Array<HttpHandler>))
+  setupServer(...([] as Array<GraphQLHandler>))
+  setupServer(...([] as Array<WebSocketHandler>))
 })

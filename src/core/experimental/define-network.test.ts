9b2a005938375438197e7f80fede3dde77be67f4
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-06 11:39:17 +0200

    test(defineNetwork): add colorless enable/disable unit tests

---
+++ src/core/experimental/define-network.test.ts
@@ -0,0 +1,89 @@
+import { NetworkSource } from './sources/network-source'
+import { defineNetwork } from './define-network'
+
+describe('enable()', () => {
+  it('returns a sync enable if all the sources are sync', () => {
+    class SyncNetworkSource extends NetworkSource {
+      enable = () => {}
+    }
+    const network = defineNetwork({
+      sources: [new SyncNetworkSource()],
+    })
+
+    expect(network.enable()).toBeUndefined()
+  })
+
+  it('returns an async enable if any the sources are async', () => {
+    class SyncNetworkSource extends NetworkSource {
+      enable = () => {}
+    }
+    class AsyncNetworkSource extends NetworkSource {
+      enable = async () => {}
+    }
+
+    const network = defineNetwork({
+      sources: [new SyncNetworkSource(), new AsyncNetworkSource()],
+    })
+
+    expect(network.enable()).toBeInstanceOf(Promise)
+  })
+
+  it('returns an async enable if all the sources are async', () => {
+    class AsyncNetworkSource extends NetworkSource {
+      enable = async () => {}
+    }
+
+    const network = defineNetwork({
+      sources: [new AsyncNetworkSource()],
+    })
+
+    expect(network.enable()).toBeInstanceOf(Promise)
+  })
+})
+
+describe('disable()', () => {
+  it('returns a sync disable if all the sources are sync', () => {
+    class SyncNetworkSource extends NetworkSource {
+      enable = () => {}
+      disable = () => {}
+    }
+    const network = defineNetwork({
+      sources: [new SyncNetworkSource()],
+    })
+
+    network.enable()
+    expect(network.disable()).toBeUndefined()
+  })
+
+  it('returns an async disable if any the sources are async', async () => {
+    class SyncNetworkSource extends NetworkSource {
+      enable = () => {}
+      disable = () => {}
+    }
+    class AsyncNetworkSource extends NetworkSource {
+      enable = async () => {}
+      disable = async () => {}
+    }
+
+    const network = defineNetwork({
+      sources: [new SyncNetworkSource(), new AsyncNetworkSource()],
+    })
+
+    await network.enable()
+    expect(network.disable()).toBeInstanceOf(Promise)
+  })
+
+  it('returns an async disable if all the sources are async', async () => {
+    class AsyncNetworkSource extends NetworkSource {
+      enable = async () => {}
+      disable = async () => {}
+    }
+
+    const network = defineNetwork({
+      sources: [new AsyncNetworkSource()],
+    })
+
+    await network.enable()
+    expect(network.disable()).toBeInstanceOf(Promise)
+  })
+})

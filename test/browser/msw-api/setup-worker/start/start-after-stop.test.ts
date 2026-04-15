1d304e358db3d3559d295d3bed6e61eaa1a96425
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-15 11:25:29 +0200

    test: add failing start-after-stop test

---
+++ test/browser/msw-api/setup-worker/start/start-after-stop.test.ts
@@ -0,0 +1,42 @@
+/**
+ * @see https://github.com/mswjs/msw/issues/2714
+ */
+import { test, expect } from '../../../playwright.extend'
+
+declare global {
+  interface Window {
+    msw: {
+      worker: import('msw/browser').SetupWorker
+    }
+  }
+}
+
+test('handles requests after starting a stopped worker', async ({
+  loadExample,
+  page,
+  fetch,
+  spyOnConsole,
+}) => {
+  const consoleSpy = spyOnConsole()
+  await loadExample(new URL('./start-after-stop.mocks.ts', import.meta.url))
+
+  await page.evaluate(() => {
+    window.msw.worker.stop()
+  })
+
+  expect(consoleSpy.get('log')).toEqual(
+    expect.arrayContaining(['[MSW] Mocking disabled.']),
+  )
+
+  await page.evaluate(async () => {
+    await window.msw.worker.start()
+  })
+
+  expect(consoleSpy.get('startGroupCollapsed')).toEqual(
+    expect.arrayContaining(['[MSW] Mocking enabled.']),
+  )
+
+  const response = await fetch('/resource')
+  expect.soft(response.status()).toBe(200)
+  await expect.soft(response.text()).resolves.toBe('hello world')
+})

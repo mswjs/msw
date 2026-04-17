973efc5ea31452b65ac7362f471b26f9142cfb6a
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-17 19:11:07 +0200

    fix: use `pagehide` instead of `beforeunload` for tracking closing pages (#2720)

--- src/browser/sources/service-worker-source.ts
+++ src/browser/sources/service-worker-source.ts
@@ -278,8 +278,12 @@ Please consider using a custom "serviceWorker.url" option to point to the actual
     this.#channel.on('RESPONSE', this.#handleResponse.bind(this))
 
     window.addEventListener(
-      'beforeunload',
-      () => {
+      'pagehide',
+      (event) => {
+        if (event.persisted) {
+          return
+        }
+
         if (worker.state !== 'redundant') {
           this.#channel.postMessage('CLIENT_CLOSED')
         }

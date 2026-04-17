edeb0585e53389875ba5e5b034854ebaa2f2f8e7
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-17 23:40:30 +0200

    chore: replace missing `ServiceWorkerIncomingRequest` with `IncomingWorkerRequest` (#2722)

--- src/browser/utils/deserializeRequest.ts
+++ src/browser/utils/deserializeRequest.ts
@@ -1,11 +1,12 @@
 import { pruneGetRequestBody } from './pruneGetRequestBody'
+import type { IncomingWorkerRequest } from './workerChannel'
 
 /**
  * Converts a given request received from the Service Worker
  * into a Fetch `Request` instance.
  */
 export function deserializeRequest(
-  serializedRequest: ServiceWorkerIncomingRequest,
+  serializedRequest: IncomingWorkerRequest,
 ): Request {
   return new Request(serializedRequest.url, {
     ...serializedRequest,

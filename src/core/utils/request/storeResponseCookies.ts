30668e68f403535636bfb8dc12c64299d9c1c6e6
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-29 11:38:40 +0200

    fix(HttpResponse): forward cookies only when response is used (#2728)

--- src/core/utils/request/storeResponseCookies.ts
+++ src/core/utils/request/storeResponseCookies.ts
@@ -1,5 +1,5 @@
 import { cookieStore } from '../cookieStore'
-import { kSetCookie } from '../HttpResponse/decorators'
+import { getRawSetCookie } from '../HttpResponse/decorators'
 
 export async function storeResponseCookies(
   request: Request,
@@ -7,9 +7,7 @@ export async function storeResponseCookies(
 ): Promise<void> {
   // Grab the raw "Set-Cookie" response header provided
   // in the HeadersInit for this mocked response.
-  const responseCookies = Reflect.get(response, kSetCookie) as
-    | string
-    | undefined
+  const responseCookies = getRawSetCookie(response)
 
   if (responseCookies) {
     await cookieStore.setCookie(responseCookies, request.url)

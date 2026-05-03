e3a9bca6e27432a6f4d195e25057b270f4b42f88
Zelys <zelys@dfkhelper.com>
2026-05-03 13:38:03 -0500

    test: fix spy timing race and cleanup leak in interceptor-source test

--- src/core/experimental/sources/interceptor-source.test.ts
+++ src/core/experimental/sources/interceptor-source.test.ts
@@ -3,31 +3,31 @@ import { FetchInterceptor } from '@mswjs/interceptors/fetch'
 import type { HttpNetworkFrame } from '../frames/http-frame'
 import { InterceptorSource } from './interceptor-source'
 
+let source: InterceptorSource
+
 afterEach(() => {
+  source?.disable()
   vi.restoreAllMocks()
 })
 
 it('removes frame event listeners after the response event is emitted', async () => {
-  const source = new InterceptorSource({
+  source = new InterceptorSource({
     interceptors: [new FetchInterceptor()],
   })
 
-  let capturedFrame: HttpNetworkFrame | undefined
+  let removeAllListenersSpy: ReturnType<typeof vi.spyOn> | undefined
 
   source.on('frame', ({ frame }) => {
-    capturedFrame = frame as unknown as HttpNetworkFrame
-    capturedFrame.respondWith(new Response('ok'))
+    const httpFrame = frame as unknown as HttpNetworkFrame
+    removeAllListenersSpy = vi.spyOn(httpFrame.events, 'removeAllListeners')
+    httpFrame.respondWith(new Response('ok'))
   })
 
   source.enable()
 
   await fetch('http://localhost/test')
 
-  const removeAllListenersSpy = vi.spyOn(capturedFrame!.events, 'removeAllListeners')
-
   await new Promise<void>((resolve) => queueMicrotask(resolve))
 
-  expect(removeAllListenersSpy).toHaveBeenCalledOnce()
-
-  source.disable()
+  expect(removeAllListenersSpy!).toHaveBeenCalledOnce()
 })
